244e646daed41fe14e3c8b9e0e77e92d8fc42c80
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-01-08 21:18:03 +0800

    string_comparison.cpp

---
+++ string_comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s = "abc";
+    
+    cout << (s=="abc") << endl;
+    cout << (s=="bac") << endl;
+    cout << (s=="d") << endl;
+
+    return 0;
+}
