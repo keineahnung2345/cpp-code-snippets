cd95b0da97beaa0d7a8385cacc0cbba61a7e7b65
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-01-08 23:15:46 +0800

    pointer_declaration_typeid.cpp

---
+++ pointer_declaration_typeid.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+int main()
+{
+    int *i1, *i2; #put one * per pointer
+    int* i3, i4;
+
+    cout << typeid(i1).name() << endl; #Pi
+    cout << typeid(i2).name() << endl; #Pi
+    cout << typeid(i3).name() << endl; #Pi
+    cout << typeid(i4).name() << endl; #i
+    
+    return 0;
+}
