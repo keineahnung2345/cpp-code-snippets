84daca3a6c734ed39cd0ff369f5ed404acd7c88a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-03-02 16:42:21 +0800

    square.cpp

---
+++ square.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    float a = 3.5;
+    
+    cout << pow(a, 2.0) << endl;
+    cout << a * a << endl;
+
+    return 0;
+}
