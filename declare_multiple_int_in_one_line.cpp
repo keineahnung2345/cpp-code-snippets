50e818e469906f6f1eb542ad8779cd8f89f74704
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-03-02 16:04:53 +0800

    declare_multiple_int_in_one_line.cpp

---
+++ declare_multiple_int_in_one_line.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+int main()
+{
+    //work!
+    int a, b = 0;
+    
+    //work!
+    int c = 0, d = 0;
+    
+    //cannot work!
+    //int e, f = 0, 0;
+
+    return 0;
+}
