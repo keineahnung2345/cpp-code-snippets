66aff9102348cd0ed485a01e521b38747e67d71c
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-03-06 23:13:23 +0800

    random seed

--- random_int.cpp
+++ random_int.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+    srand(1); //set random seed for reproducible result
     // this will generate numbers from 3 to 3+5=8
     for(int i = 0; i < 10; i++){
         cout << rand()%5+3 << " ";
