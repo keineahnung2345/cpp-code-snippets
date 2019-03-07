d2528e044374b19e5680e23b2e64eb4a76b80595
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-03-07 21:38:24 +0800

    vector_concatenate.cpp

---
+++ vector_concatenate.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> v1 {1,2,3,4,5,6};
+    vector<int> v2 {7,8,9,10};
+    
+    v1.insert(v1.end(), v2.begin(), v2.end());
+    
+    for(auto e : v1){
+        cout << e << " ";
+    }
+    cout << endl;
+}
