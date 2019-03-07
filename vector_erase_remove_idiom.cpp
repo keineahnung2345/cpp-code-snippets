0e0855be99dbcc06b60f3e651da4ef1b296a8e11
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-03-07 17:20:56 +0800

    vector_erase_remove_idiom.cpp

---
+++ vector_erase_remove_idiom.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> //remove
+
+using namespace std;
+
+
+//https://en.cppreference.com/w/cpp/algorithm/remove
+//https://stackoverflow.com/questions/347441/erasing-elements-from-a-vector
+
+int main()
+{
+    vector<char> v = {'a', 'b', 'c', 'a', 'd', 'b'};
+    
+    //erase-remove idiom
+    v.erase(remove(v.begin(), v.end(), 'b'), v.end());
+    
+    for(auto e : v){
+        cout << e << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
+
