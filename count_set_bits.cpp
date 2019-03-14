070bc8f25b26955b142ae1a1ec263554116bc667
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-03-14 22:33:08 +0800

    count_set_bits.cpp

---
+++ count_set_bits.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main()
+{
+    bitset<20> b(20);
+    cout << b << endl;
+    cout << b.count() << endl;
+
+    return 0;
+}
