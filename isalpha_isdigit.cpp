c2cd2f5ed94b2d45a7831192dcbec37847b1d2f7
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-03-15 22:21:27 +0800

    isalpha_isdigit.cpp

---
+++ isalpha_isdigit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << isalpha('a') << endl;
+    cout << isdigit('1') << endl;
+    
+    //isalpha and isdigit can only accept char
+    // cout << isalpha("abc") << endl;
+    return 0;
+}
