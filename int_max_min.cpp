f8729836829b7596f4fb5925ef9d7f6efedbfa45
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-03-27 22:51:36 +0800

    int_max_min.cpp

---
+++ int_max_min.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+//http://www.cplusplus.com/reference/climits/
+
+int main()
+{
+    
+    int a = INT_MAX, b = INT_MIN;
+    cout << a << " " << b << endl; //2147483647 -2147483648 
+    return 0;
+}
