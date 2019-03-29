a8156aff5c351891ea519797b65c5016017aefd0
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-03-29 13:54:38 +0800

    add string.compare

--- string_comparison.cpp
+++ string_comparison.cpp
@@ -9,6 +9,11 @@ int main()
     cout << (s=="abc") << endl;
     cout << (s=="bac") << endl;
     cout << (s=="d") << endl;
-
+    
+    cout << string("hello").compare("world") << endl; //-15
+    cout << string("A").compare("Ab") << endl; //-1
+    cout << string("AB").compare("CD") << endl; //-2
+    cout << string("ABC").compare("ABC") << endl; //0
+    cout << string("harry").compare("happy") << endl; //2
     return 0;
 }
