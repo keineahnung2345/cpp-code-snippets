ba68382b045cfd9f8df74ae43b3bad17bc76c803
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-03-30 16:04:05 +0800

    stack_to_vector.cpp

---
+++ stack_to_vector.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+//https://stackoverflow.com/questions/4346010/copy-stdstack-into-an-stdvector
+int main()
+{
+    stack<int> stk;
+    stk.push(1);
+    stk.push(4);
+    stk.push(9);
+    stk.push(3);
+    
+    int* end   = &stk.back() + 1;
+    int* begin = end - stk.size();
+    vector<int> stack_contents(begin, end);
+    
+    //1 4 9 3
+    for(int e : stack_contents){
+        cout << e << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
