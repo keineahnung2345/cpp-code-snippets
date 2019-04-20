3480b7a23457c84e8deaa5ac8469e14a607bb63a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-04-20 15:40:55 +0800

    add examples

--- string_rfind.cpp
+++ string_rfind.cpp
@@ -8,6 +8,13 @@ int main ()
   string str ("The sixth sick sheik's sixth sheep's sick.");
   string key ("sixth");
 
+  //start from last char and go back
+  cout << str.rfind(key) << endl; //23
+  //start from 5th char and go back
+  cout << str.rfind(key, 5) << endl;//4
+  //start from 0 and go back->cannot find anything
+  cout << (str.rfind(key, 0) == string::npos) << endl; //1
+  
   size_t found = str.rfind(key);
   if (found!=string::npos)
     str.replace (found,key.length(),"seventh");
