004fdaf09d508ac32095b306495fc2eb8747b4fd
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-08-09 10:04:46 +0800

    sort_with_custom_comparator.cpp

---
+++ sort_with_custom_comparator.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> //sort
+#include <iterator> //ostream_iterator
+
+class Point{
+    public:
+        int x;
+        int y;
+        Point(int x_in, int y_in){
+            x = x_in;
+            y = y_in;
+        };
+        ~Point(){};
+};
+
+static bool point_comparator(Point &a, Point &b)
+{
+    //the smaller the former
+    return a.x * a.x + a.y * a.y < b.x * b.x + b.y * b.y;
+}
+
+int main()
+{
+    Point a(2,3);
+    Point b(1,4);
+    
+    std::cout << "(2, 3) smaller than (1, 4): " << point_comparator(a, b) << std::endl;
+    
+    std::vector<Point> v = {Point(2,3), Point(1,4), Point(0,5), Point(-1,6)};
+    
+    std::sort(v.begin(), v.end(), point_comparator);
+    
+    std::cout << "sorted vector of Point: " << std::endl;
+    for(std::vector<Point>::iterator it = v.begin(); it != v.end(); it++){
+        std::cout << it->x << " " << it->y << std::endl;
+    }
+
+    return 0;
+}
