143dd4299380a8275e76cd525a3b302c1560a9cf
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-09-06 09:24:26 +0800

    file_exist.cpp

---
+++ file_exist.cpp
@@ -0,0 +1,30 @@
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string>
+#include <fstream>
+
+//https://stackoverflow.com/questions/12774207/fastest-way-to-check-if-a-file-exist-using-standard-c-c11-c
+//the later the faster
+
+inline bool exists_test0 (const std::string& name) {
+    ifstream f(name.c_str());
+    return f.good();
+}
+
+inline bool exists_test1 (const std::string& name) {
+    if (FILE *file = fopen(name.c_str(), "r")) {
+        fclose(file);
+        return true;
+    } else {
+        return false;
+    }   
+}
+
+inline bool exists_test2 (const std::string& name) {
+    return ( access( name.c_str(), F_OK ) != -1 );
+}
+
+inline bool exists_test3 (const std::string& name) {
+  struct stat buffer;   
+  return (stat (name.c_str(), &buffer) == 0); 
+}
