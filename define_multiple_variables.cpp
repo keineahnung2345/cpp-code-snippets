df99ba3ad452b2a954abdc67109e44c1e1b077af
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-09-30 13:50:01 +0800

    define_multiple_variables.cpp

---
+++ define_multiple_variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+//https://stackoverflow.com/questions/6838408/how-can-i-declare-and-define-multiple-variables-in-one-line-using-c/6838480
+
+int main()
+{
+    int a = 1, b = 2, c = 3;
+    cout << a << ", " << b << ", " << c << endl;
+    return 0;
+}
