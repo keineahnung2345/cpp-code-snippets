28dd38f279683e1d7620f59946596f08d09ffa4a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-10-29 11:06:03 +0800

    draw rect method 2

--- opencv/opencv_draw_rectangle.cpp
+++ opencv/opencv_draw_rectangle.cpp
@@ -11,6 +11,11 @@ int main()
     int width = 10;
     int height = 20;
 
+    //method 1
+    cv::Point pt1(x, y);
+    cv::Point pt2(x + width, y + height);
+    cv::rectangle(img, pt1, pt2, cv::Scalar(0, 255, 0));
+    //method 2
     cv::Rect rect(x, y, width, height);
     cv::rectangle(img, rect, cv::Scalar(0, 255, 0));
     return 0;
