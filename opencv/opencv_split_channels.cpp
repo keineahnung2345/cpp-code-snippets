c65e4acb89f4c450db9e5cb8ed9edc2092ffb534
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-11-05 10:05:24 +0800

    opencv_split_channels.cpp

---
+++ opencv/opencv_split_channels.cpp
@@ -0,0 +1,21 @@
+#include <opencv2/opencv.hpp>
+
+//g++ opencv_split_channels.cpp `pkg-config --cflags --libs opencv`
+
+int main(){
+    cv::Mat img = cv::imread("xxx.jpeg");
+    std::cout << "image size: " << img.size << ", channels: " << img.channels() << std::endl;
+    cv::Mat ch1, ch2, ch3;
+    // "channels" is a vector of 3 Mat arrays:
+    std::vector<cv::Mat> channels(3);
+    // split img:
+    cv::split(img, channels);
+    // get the channels (dont forget they follow BGR order in OpenCV)
+    ch1 = channels[0];
+    ch2 = channels[1];
+    ch3 = channels[2];
+    std::cout << "after split, one channel's size: " << ch1.size << ", channels: " << ch1.channels() << std::endl;
+}
+
+//image size: 173 x 292, channels: 3
+//after split, one channel's size: 173 x 292, channels: 1
