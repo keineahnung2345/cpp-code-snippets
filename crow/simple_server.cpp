e43206db03f140a88f16cd5d395d4f6b62fe62bb
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-11-14 13:49:10 +0800

    set log level

--- crow/simple_server.cpp
+++ crow/simple_server.cpp
@@ -3,6 +3,8 @@
 //export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:<boost_installation_dir>/lib
 //g++ main.cpp -I <crow_dir>/amalgamate -L <boost_installation_dir>/lib -lboost_system -lboost_thread -lpthread -std=c++11
 
+//https://github.com/ipkn/crow/blob/master/examples/example.cpp
+
 int main()
 {
     crow::SimpleApp app;
@@ -10,6 +12,8 @@ int main()
     CROW_ROUTE(app, "/test").methods("POST"_method)([](){
         return "Hello world";
     });
-
+    
+    // enables all log
+    app.loglevel(crow::LogLevel::DEBUG);
     app.port(18080).multithreaded().run();
 }
