1a0565e096bca20e7fa5f34aca75e91d4f8d9319
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-11-27 11:46:21 +0800

    map_new_key_default_value.cpp

---
+++ map_new_key_default_value.cpp
@@ -0,0 +1,27 @@
+#include <map>
+#include <iostream>
+#include <string>
+
+template <typename T>
+void print_map(T m){
+    for(auto it = m.begin(); it != m.end(); it++){
+        std::cout << it->first << " " << it->second << std::endl;
+    }
+}
+
+int main(){
+    std::map<int, int> m_i;
+    std::map<int, std::string> m_s;
+
+    m_i[0]++;
+    m_s[0] += 'a';
+
+    print_map(m_i);
+    print_map(m_s);
+    return 0;
+}
+
+/*
+0 1
+0 a
+*/
