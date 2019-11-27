84ba0e5fb0790922dbd1204325e0cfa6bcf0b648
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-11-27 17:15:50 +0800

    exponential.cpp

---
+++ exponential.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main(){
+    std::cout << "1e3: " << 1e3 << std::endl;
+    std::cout << "10e-3: " << 10e-3 << std::endl;
+    std::cout << "10e-2: " << 10e-2 << std::endl;
+    std::cout << "10e-1: " << 10e-1 << std::endl;
+    std::cout << "10e0: " << 10e0 << std::endl;
+    std::cout << "10e1: " << 10e1 << std::endl;
+    std::cout << "10e2: " << 10e2 << std::endl;
+    std::cout << "10e3: " << 10e3 << std::endl;
+    std::cout << "2e0: " << 2e0 << std::endl;
+    std::cout << "2e1: " << 2e1 << std::endl;
+    std::cout << "2e2: " << 2e2 << std::endl;
+    return 0;
+}
+
+/*
+1e3: 1000
+10e-3: 0.01
+10e-2: 0.1
+10e-1: 1
+10e0: 10
+10e1: 100
+10e2: 1000
+10e3: 10000
+2e0: 2
+2e1: 20
+2e2: 200
+*/
