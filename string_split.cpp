0d30c2e59d0550537712e38f841cd6c678c2f5b7
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-11-28 15:34:30 +0800

    make it a function

--- string_split.cpp
+++ string_split.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
-
-using namespace std;
+#include <vector>
+#include <string>
 
 //https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
+
+std::vector<std::string> string_split(std::string str, std::string delimiter){
+    size_t pos = 0;
+    std::string token;
+    std::vector<std::string> result;
+    while ((pos = str.find(delimiter)) != std::string::npos) {
+        token = str.substr(0, pos);
+        result.push_back(token);
+        str.erase(0, pos + delimiter.length());
+    }
+    result.push_back(str);
+    return result;
+}
+
 int main()
 {
-    string s = "scott>=tiger>=mushroom";
-    string delimiter = ">=";
+    std::string str = "scott>=tiger>=mushroom";
+    std::string delimiter = ">=";
     
-    size_t pos = 0;
-    string token;
-    while ((pos = s.find(delimiter)) != string::npos) {
-        token = s.substr(0, pos);
-        cout << token << endl;
-        s.erase(0, pos + delimiter.length());
+    for(std::string token : string_split(str, delimiter)){
+        std::cout << token << std::endl;
     }
-    cout << s << endl;
 
     return 0;
 }
-
-/**
-output: 
-scott
-tiger
-mushroom
-**/
