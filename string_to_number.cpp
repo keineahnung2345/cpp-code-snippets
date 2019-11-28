8d02d0297878d0ca52757cf584fce21f05a82198
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-11-28 15:54:29 +0800

    string_to_number.cpp

--- string_to_number.cpp
+++ string_to_number.cpp
@@ -4,8 +4,12 @@
 #include <sstream>
 
 //https://stackoverflow.com/questions/29200635/convert-float-to-string-with-precision-number-of-decimal-digits-specified
+//http://www.cplusplus.com/reference/string/stoi/
+
+//g++ string_to_number.cpp -std=c++11
 
 int main(){
+    //to double
     std::string sd = "123.456789123";
     std::cout << std::stod(sd) << std::endl; //123.457
     std::cout << std::stold(sd) << std::endl; //123.457
@@ -13,5 +17,9 @@ int main(){
     ss << std::fixed << std::setprecision(10) << sd;
     std::string str = ss.str();
     std::cout << str << std::endl; //123.456789123
+
+    //to int
+    std::string si = "123456";
+    std::cout << std::stoi(si, nullptr) << std::endl;
     return 0;
 }
