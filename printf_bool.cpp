1fe3b46a78b55154dab71c7fd0782007a761c073
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-11-28 17:42:49 +0800

    add ref

--- printf_bool.cpp
+++ printf_bool.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+//https://stackoverflow.com/questions/17307275/what-is-the-printf-format-specifier-for-bool
+
 int main(){
     bool x = true;
     printf("%s\n", x ? "true" : "false");
