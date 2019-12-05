0cf8c1017db2784c8b01f516cd1be267cdeb97db
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-12-05 15:14:25 +0800

    %li for long

--- printf_long.cpp
+++ printf_long.cpp
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
 //https://stackoverflow.com/questions/38561/what-is-the-argument-for-printf-that-formats-a-long
+//https://stackoverflow.com/questions/2258700/how-do-i-printf-a-long-shouldnt-this-work-li
 
 int main(){
     long l = -123456789123;
     unsigned long lu = 1234567891234;
-    printf("%ld\n", l);
+    printf("%ld %ld\n", l, lu);
+    printf("%li %li\n", l, lu);
     printf("%lu\n", lu);
+    /*
+    -123456789123 1234567891234
+    -123456789123 1234567891234
+    1234567891234
+    */
     return 0;
 }
