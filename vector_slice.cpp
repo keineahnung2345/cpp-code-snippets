5eb8039fb75cc9407d90b41ea614f72957f519e1
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-12-12 18:01:00 +0800

    add ref

--- vector_slice.cpp
+++ vector_slice.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 #include <iostream>
 
+//https://stackoverflow.com/questions/50549611/slicing-a-vector-in-c
+
 int main(){
     std::vector<int> v1 = {1, 2, 3, 4, 5};
     std::vector<int> v2(v1.begin()+1, v1.end()-1);
