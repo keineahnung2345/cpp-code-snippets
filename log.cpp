21a294f8d04dc3c0f7e972d3f11d6295ce277260
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-12-15 00:30:54 +0800

    log.cpp

---
+++ log.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <math.h>
+
+int main(){
+    std::cout << log2(15) << "->" << int(log2(15)) << std::endl;
+    std::cout << log2(16) << "->" << int(log2(16)) << std::endl;
+    return 0;
+}
