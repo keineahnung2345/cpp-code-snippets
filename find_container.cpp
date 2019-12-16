9041857fa456c034c81883518d2100fcaef67c8c
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-12-16 10:14:34 +0800

    find all

--- find_container.cpp
+++ find_container.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm> //find
-using namespace std;
 
 int main()
 {
-    vector<int> v = {1, 2, 3, 4, 5, 6};
-    cout << *find(v.begin(), v.end(), 5);
-
+    std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 5, 2, 5};
+    std::vector<int>::iterator it = std::find(v.begin(), v.end(), 5);
+    while(it != v.end()){
+        std::cout << "index: " << it - v.begin() << ", value: " << *it << std::endl;
+        it = std::find(it+1, v.end(), 5);
+    }
     return 0;
 }
