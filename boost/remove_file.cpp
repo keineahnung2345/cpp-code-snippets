eb25cbb6b8bb2806269341f18a3c722a4cee1bee
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-01-03 16:22:13 +0800

    remove_file.cpp

---
+++ boost/remove_file.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <boost/filesystem.hpp>
+
+//g++ remove_file.cpp -lboost_filesystem -lboost_system
+/*-lboost_system is required, otherwise there will be:
+//usr/bin/ld: /tmp/ccDwa2Ug.o: undefined reference to symbol '_ZN5boost6system15system_categoryEv'
+//usr/lib/x86_64-linux-gnu/libboost_system.so.1.58.0: error adding symbols: DSO missing from command line
+collect2: error: ld returned 1 exit status
+*/
+
+int main(){
+    std::string fname = "tmp.txt";
+    boost::filesystem::path fname_path(fname);
+    //return true if the file exist, false if not exist
+    bool success = boost::filesystem::remove(fname_path);
+    std::cout << "remove " << fname << "?: ";
+    std::cout << (success ? "true" : "false") << std::endl;
+    return 0;
+}
