2796b49828d3b433fb2a25ca328217d7817ff0d7
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-01-07 10:19:26 +0800

    template - utils.h

---
+++ template/utils.h
@@ -0,0 +1,19 @@
+#include <sstream> //std::ostringstream
+
+#ifdef CORRECT
+template <typename Iter>
+std::string join(Iter begin, Iter end, std::string const& separator)
+{
+    std::ostringstream result;
+    result.precision(2); //for floating point
+    if (begin != end)
+        result << *begin++;
+    while (begin != end)
+        //std::fixed : for floating point
+        result << std::fixed << separator << *begin++;
+    return result.str();
+}
+#else
+template <typename Iter>
+std::string join(Iter begin, Iter end, std::string const& separator);
+#endif
