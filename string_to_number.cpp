e8494a6a014835a2652795b47fdd9327d910b271
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-01-08 10:56:01 +0800

    stof

--- string_to_number.cpp
+++ string_to_number.cpp
@@ -18,8 +18,14 @@ int main(){
     std::string str = ss.str();
     std::cout << str << std::endl; //123.456789123
 
+    //to float
+    std::string sf = "123.456";
+    std::cout << std::stof(sf) << std::endl; //123.456
+
     //to int
     std::string si = "123456";
-    std::cout << std::stoi(si, nullptr) << std::endl;
+    std::cout << std::stoi(si, nullptr) << std::endl; //123456
+    std::cout << std::stoi(si) << std::endl; //123456
+
     return 0;
 }
