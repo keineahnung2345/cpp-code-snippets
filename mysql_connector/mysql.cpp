8a8020baaea10cbf0acf9d60720f405279ae20ea
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-01-10 11:38:23 +0800

    add example

---
+++ mysql_connector/mysql.cpp
@@ -0,0 +1,84 @@
+/* Copyright 2008, 2010, Oracle and/or its affiliates. All rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+There are special exceptions to the terms and conditions of the GPL
+as it is applied to this software. View the full text of the
+exception in file EXCEPTIONS-CONNECTOR-C++ in the directory of this
+software distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+/* Standard C++ includes */
+#include <stdlib.h>
+#include <iostream>
+
+/*
+  Include directly the different
+  headers from cppconn/ and mysql_driver.h + mysql_util.h
+  (and mysql_connection.h). This will reduce your build time!
+*/
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
+using namespace std;
+
+//https://archive.md/TeGBj#selection-2819.1-2847.1
+//g++ mysql.cpp -I/usr/include/mysql-cppconn-8/jdbc -lmysqlcppconn
+
+int main(void)
+{
+    cout << "Running 'SHOW COLUMNS FROM users' in performance_schema..." << endl;
+    
+    try {
+        sql::Driver *driver;
+        sql::Connection *con;
+        sql::Statement *stmt;
+        sql::ResultSet *res;
+    
+        /* Create a connection */
+        driver = get_driver_instance();
+        con = driver->connect("tcp://127.0.0.1:3306", "root", "jianbox");
+        /* Connect to the MySQL test database */
+        con->setSchema("performance_schema");
+    
+        stmt = con->createStatement();
+        res = stmt->executeQuery("SHOW COLUMNS FROM users");
+        while (res->next()) {
+         cout << res->getString(1) << endl;
+        }
+        delete res;
+        delete stmt;
+        delete con;
+    
+    } catch (sql::SQLException &e) {
+        cout << "# ERR: SQLException in " << __FILE__;
+        cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+        cout << "# ERR: " << e.what();
+        cout << " (MySQL error code: " << e.getErrorCode();
+        cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+    }
+    
+    return EXIT_SUCCESS;
+}
+
+/*
+Running 'SHOW COLUMNS FROM users' in performance_schema...
+USER
+CURRENT_CONNECTIONS
+TOTAL_CONNECTIONS
+*/
