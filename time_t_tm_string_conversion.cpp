67c7fad64ad536e0a2fc456292bd278c0827be7e
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-01-14 13:59:28 +0800

    time_t_tm_string_conversion.cpp

--- time_t_tm_string_conversion.cpp
+++ time_t_tm_string_conversion.cpp
@@ -6,13 +6,15 @@
 //https://www.runoob.com/w3cnote/cpp-time_t.html
 //https://bryceknowhow.blogspot.com/2013/10/cc-gmtimemktimetimetstruct-tm.html
 //https://stackoverflow.com/questions/16344748/why-gmtime-and-localtime-give-me-the-same-result
+//https://stackoverflow.com/questions/11213326/how-to-convert-a-string-variable-containing-time-to-time-t-type-in-c
 
 int main()
 {
     time_t timer, timer_back;
-    struct tm *tm_info_tmp, tm_info_local, tm_info_gm;
-    struct tm tb;
-    char buffer[100];
+    struct tm *tm_info_tmp;
+    struct tm tm_info_local, tm_info_gm;
+    struct tm tm_info_local_back, tm_info_gm_back;
+    char str_local[100], str_gm[100];
     char fmt[] = "%Y-%m-%d-%H:%M:%S";
 
     time(&timer);
@@ -25,26 +27,31 @@ int main()
     memcpy(&tm_info_local, tm_info_tmp, sizeof(tm_info_local));
 
     printf("GMT tm                         :");
-    strftime(buffer, 100, fmt, &tm_info_gm);
-    puts(buffer);
+    strftime(str_gm, 100, fmt, &tm_info_gm);
+    puts(str_gm);
 
     printf("local time tm                  :");
-    strftime(buffer, 100, fmt, &tm_info_local);
-    puts(buffer);
+    strftime(str_local, 100, fmt, &tm_info_local);
+    puts(str_local);
 
-    timer_back = mktime(&tm_info_gm);
+    //from string to struct tm
+    strptime(str_gm, fmt, &tm_info_gm_back);
+    strptime(str_local, fmt, &tm_info_local_back);
+
+    //from struct tm to time_t
+    timer_back = mktime(&tm_info_gm_back);
     printf("time_t back from GMT tm        :%ld, %s", (long int)timer_back, ctime(&timer_back));
 
-    timer_back = mktime(&tm_info_local);
+    timer_back = mktime(&tm_info_local_back);
     printf("time_t back from local time tm :%ld, %s", (long int)timer_back, ctime(&timer_back));
 
     return 0;
 }
 
 /*
-time_t                         :1578980628, Tue Jan 14 13:43:48 2020
-GMT tm                         :2020-01-14-05:43:48
-local time tm                  :2020-01-14-13:43:48
-time_t back from GMT tm        :1578951828, Tue Jan 14 05:43:48 2020
-time_t back from local time tm :1578980628, Tue Jan 14 13:43:48 2020
+time_t                         :1578981505, Tue Jan 14 13:58:25 2020
+GMT tm                         :2020-01-14-05:58:25
+local time tm                  :2020-01-14-13:58:25
+time_t back from GMT tm        :1578952705, Tue Jan 14 05:58:25 2020
+time_t back from local time tm :1578981505, Tue Jan 14 13:58:25 2020
 */
