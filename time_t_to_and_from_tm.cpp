c0fb9138537a3eafa9d2acd2b0b79ac6bf485016
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-01-14 13:44:37 +0800

    add "time_t back from local time tm"

--- time_t_to_and_from_tm.cpp
+++ time_t_to_and_from_tm.cpp
@@ -16,7 +16,7 @@ int main()
     char fmt[] = "%Y-%m-%d-%H:%M:%S";
 
     time(&timer);
-    printf("time_t         :%ld, %s",(long int)timer, ctime(&timer));
+    printf("time_t                         :%ld, %s",(long int)timer, ctime(&timer));
 
     tm_info_tmp = gmtime(&timer);
     //need to copy the value from tm_info_tmp to another variable, ow it will be overwritten later
@@ -24,23 +24,27 @@ int main()
     tm_info_tmp = localtime(&timer);
     memcpy(&tm_info_local, tm_info_tmp, sizeof(tm_info_local));
 
-    printf("GMT tm         :");
+    printf("GMT tm                         :");
     strftime(buffer, 100, fmt, &tm_info_gm);
     puts(buffer);
 
-    printf("local time tm  :");
+    printf("local time tm                  :");
     strftime(buffer, 100, fmt, &tm_info_local);
     puts(buffer);
 
     timer_back = mktime(&tm_info_gm);
-    printf("time_t back    :%ld, %s", (long int)timer_back, ctime(&timer_back));
+    printf("time_t back from GMT tm        :%ld, %s", (long int)timer_back, ctime(&timer_back));
+
+    timer_back = mktime(&tm_info_local);
+    printf("time_t back from local time tm :%ld, %s", (long int)timer_back, ctime(&timer_back));
 
     return 0;
 }
 
 /*
-time_t         :1578974895, Tue Jan 14 12:08:15 2020
-GMT tm         :2020-01-14-04:08:15
-local time tm  :2020-01-14-12:08:15
-time_t back    :1578946095, Tue Jan 14 04:08:15 2020
+time_t                         :1578980628, Tue Jan 14 13:43:48 2020
+GMT tm                         :2020-01-14-05:43:48
+local time tm                  :2020-01-14-13:43:48
+time_t back from GMT tm        :1578951828, Tue Jan 14 05:43:48 2020
+time_t back from local time tm :1578980628, Tue Jan 14 13:43:48 2020
 */
