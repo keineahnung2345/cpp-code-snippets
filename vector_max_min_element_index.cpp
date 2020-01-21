db21184b94099c3c0339eba2b27c7d26eccfd8e1
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-01-21 16:07:05 +0800

    max among a specific range

--- vector_max_min_element_index.cpp
+++ vector_max_min_element_index.cpp
@@ -11,11 +11,13 @@ int main() {
 
     cout << "max's index: " << std::distance(myvec.begin(), std::max_element(myvec.begin(), myvec.end())) << ", value: " << *std::max_element(myvec.begin(), myvec.end()) << endl;
     cout << "min's index: " << std::distance(myvec.begin(), std::min_element(myvec.begin(), myvec.end())) << ", value: " << *std::min_element(myvec.begin(), myvec.end()) << endl;
-
-    /*
-    max's index: 4, value: 9
-    min's index: 2, value: 1
-    */
-
+    // tail is not inclusive, so we need +1 here
+    cout << "max among [0:3] , index : " << std::distance(myvec.begin(), std::max_element(myvec.begin(), myvec.begin() + 3 + 1)) << ", value: " << *std::max_element(myvec.begin(), myvec.begin() + 3 + 1) << endl;
     return 0;
 }
+
+/*
+max's index: 4, value: 9
+min's index: 2, value: 1
+max among [0:3] , index : 3, value: 7
+*/
