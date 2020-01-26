66b3b6dd049e44809c7cc2310f88468455a71d69
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-01-26 19:50:26 +0800

    vector_emplace_back_object.cpp

---
+++ vector_emplace_back_object.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+class Rectangle{
+public:
+    Rectangle(int h, int w): height(h), width(w){
+        area = height * width;
+    };
+
+    int get_area(){
+        return area;
+    }
+private:
+    int area;
+    int height;
+    int width;
+};
+
+int main(){
+    std::vector<Rectangle> rects;
+
+    rects.push_back(Rectangle(10,20));
+    rects.emplace_back(20,30);
+
+    std::cout << rects[0].get_area() << std::endl;
+    std::cout << rects[1].get_area() << std::endl;
+    
+    return 0;
+}
