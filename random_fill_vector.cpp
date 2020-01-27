fdcea8a1ab83ad791574d6253b55e7c50b9c18b9
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-01-27 11:58:30 +0800

    random fill vector

---
+++ random_fill_vector.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+
+int main(){
+    std::vector<int> v(10);
+    std::generate(v.begin(), v.end(), std::rand);
+    for(size_t i = 0; i < v.size(); i++){
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
+
+//16807 282475249 1622650073 984943658 1144108930 470211272 101027544 1457850878 1458777923 2007237709
