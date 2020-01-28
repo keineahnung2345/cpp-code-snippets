d703f62000e0310753f1525058ae274efa434ff0
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-01-28 21:20:02 +0800

    assert(0 && "")

--- assert_with_custom_message.cpp
+++ assert_with_custom_message.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <assert.h>
+#include <vector>
 
 //https://stackoverflow.com/questions/3692954/add-custom-messages-in-assert
 
@@ -8,9 +9,18 @@ int main(){
     //assert(a == b && "A is not equal to B");
     //a.out: assert_message.cpp:6: int main(): Assertion `a == b && "A is not equal to B"' failed.
     //Aborted (core dumped)
-    
-    assert(("A must be equal to B", a == b));
+
+    //assert(("A must be equal to B", a == b));
     //a.out: assert_message.cpp:7: int main(): Assertion `("A must be equal to B", a == b)' failed.
     //Aborted (core dumped)
+
+    //on Mac
+    //Assertion failed: (0 && "vector index must in [0, v.size())"), function main, file assert.cpp, line 20.
+    //Abort trap: 6
+    std::vector<int> v = {1,2,3};
+    int index = -1;
+    if(index < 0 || index >= v.size()){
+        assert(0 && "vector index must be in [0, v.size())");
+    }
     return 0;
 }
