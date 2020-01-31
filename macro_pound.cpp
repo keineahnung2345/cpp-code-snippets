128d86bbd2d40e8aa8cf52868b97764455675816
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-01-31 10:49:03 +0800

    macro_pound.cpp

---
+++ macro_pound.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+  
+#ifdef _MSC_VER
+#define FN_NAME __FUNCTION__
+#else
+#define FN_NAME __func__
+#endif
+
+#define NULLPTR
+
+#define CHECK_NULL_RET_VAL(ptr, val)                                                           \
+    if ((ptr) == nullptr)                                                                      \
+    {                                                                                          \
+        std::cout << "Error: input " << #ptr << " is NULL in " << FN_NAME << std::endl;        \
+        return val;                                                                            \
+    }
+
+//https://stackoverflow.com/questions/14351971/what-does-x-inside-a-c-macro-mean
+
+int main(){
+#ifdef NULLPTR
+    int* p = nullptr;
+    int i = 3;
+#else
+    int* p = &i;
+#endif
+    CHECK_NULL_RET_VAL(p, 0)
+    return 0;
+}
+
+//Error: input p is NULL in main
