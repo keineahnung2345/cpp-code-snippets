0c7af64d921f340e3eaa3b07ae65d1c063d2740b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-04 18:04:32 +0800

    add typedef sample

--- function_pointer.cpp
+++ function_pointer.cpp
@@ -10,12 +10,17 @@ int main(){
     int (*fnc_ptr)(int) = &square;
     std::cout << (*fnc_ptr)(10) << std::endl;
 
-    int (*fnc_ptr2)(int) = square;
+    int(*fnc_ptr2)(int) = square;
     std::cout << fnc_ptr2(10) << std::endl;
+
+    typedef int(*MyFnc)(int);
+    MyFnc f = square;
+    std::cout << f(10) << std::endl;
     return 0;
 }
 
 /*
+100
 100
 100
 */
