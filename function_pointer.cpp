56de500b1e8e275ab0b8e5f56299fab54d034e82
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-04 18:07:40 +0800

    add template sample

--- function_pointer.cpp
+++ function_pointer.cpp
@@ -7,19 +7,28 @@ int square(int x){
 //https://www.geeksforgeeks.org/function-pointer-in-c/
 
 int main(){
+    //use & and *
     int (*fnc_ptr)(int) = &square;
     std::cout << (*fnc_ptr)(10) << std::endl;
 
+    //not using & and *
     int(*fnc_ptr2)(int) = square;
     std::cout << fnc_ptr2(10) << std::endl;
 
+    //typedef
     typedef int(*MyFnc)(int);
     MyFnc f = square;
     std::cout << f(10) << std::endl;
+  
+    //function pointer as template argument
+    std::unordered_map<std::string, MyFnc> mymap;
+    mymap["square"] = square;
+    std::cout << mymap["square"](10) << std::endl;
     return 0;
 }
 
 /*
+100
 100
 100
 100
