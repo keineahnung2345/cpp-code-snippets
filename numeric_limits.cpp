df5e785394569c65d9c832e6b0c8632fe22175ad
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-05 11:48:43 +0800

    add int

--- numeric_limits.cpp
+++ numeric_limits.cpp
@@ -1,31 +1,42 @@
 #include <iostream>
+#include <limits>       // std::numeric_limits
 
 //http://www.cplusplus.com/reference/limits/numeric_limits/
 
 int main(){
-    std::cout << "max, min, lowest for 3 different data types: " << std::endl;
+    std::cout << "max, min, lowest for 4 different data types: " << std::endl;
+    std::cout << "\nint: " << std::endl;
+    std::cout << std::numeric_limits<int>::max() << std::endl;
+    std::cout << std::numeric_limits<int>::min() << std::endl;
+    std::cout << std::numeric_limits<int>::lowest() << std::endl;
+    
     std::cout << "\nfloat: " << std::endl;
     std::cout << std::numeric_limits<float>::max() << std::endl;
     std::cout << std::numeric_limits<float>::min() << std::endl;
     std::cout << std::numeric_limits<float>::lowest() << std::endl;
-
+    
     std::cout << "\ndouble: " << std::endl;
     std::cout << std::numeric_limits<double>::max() << std::endl;
     std::cout << std::numeric_limits<double>::min() << std::endl;
     std::cout << std::numeric_limits<double>::lowest() << std::endl;
-
+    
     std::cout << "\nlong double: " << std::endl;
     std::cout << std::numeric_limits<long double>::max() << std::endl;
     std::cout << std::numeric_limits<long double>::min() << std::endl;
     std::cout << std::numeric_limits<long double>::lowest() << std::endl;
-
+    
     return 0;
 }
 
 /*
-max, min, lowest for 3 different data types:
+max, min, lowest for 4 different data types:
 
-float: 
+int:
+2147483647
+-2147483648
+-2147483648
+
+float:
 3.40282e+38
 1.17549e-38
 -3.40282e+38
