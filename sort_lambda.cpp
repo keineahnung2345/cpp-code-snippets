7447e71f68b4701e92c5dc5762d63d06c2690a96
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-22 23:32:16 +0800

    sort_lambda.cpp

---
+++ sort_lambda.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+//https://stackoverflow.com/questions/5122804/how-to-sort-with-a-lambda
+
+int main(){
+    std::vector<std::string> myvec;
+    myvec = {"apple", "lemon", "orange", "banana", "onion", "melon", "durian"};
+    std::sort(myvec.begin(), myvec.end(),
+        [](const std::string& a, const std::string& b) -> bool{return a.size() == b.size() ? a < b : a.size() > b.size();});
+    std::copy(myvec.begin(), myvec.end(), std::ostream_iterator<std::string>(std::cout, " "));
+    std::cout << std::endl;
+}
+
+//banana durian orange apple lemon melon onion
