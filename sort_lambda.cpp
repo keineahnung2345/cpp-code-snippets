a2050143bdc11043bef65f8700319a334207cf71
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-22 23:37:38 +0800

    add comment

--- sort_lambda.cpp
+++ sort_lambda.cpp
@@ -7,6 +7,7 @@
 int main(){
     std::vector<std::string> myvec;
     myvec = {"apple", "lemon", "orange", "banana", "onion", "melon", "durian"};
+    //first sort by length(the longer the former), then by lexicographical order
     std::sort(myvec.begin(), myvec.end(),
         [](const std::string& a, const std::string& b) -> bool{return a.size() == b.size() ? a < b : a.size() > b.size();});
     std::copy(myvec.begin(), myvec.end(), std::ostream_iterator<std::string>(std::cout, " "));
