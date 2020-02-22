658612aacc1664cfa2991937beda1910e3d7ea2c
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-22 12:16:33 +0800

    min for multiple argument

--- min_max.cpp
+++ min_max.cpp
@@ -3,8 +3,12 @@
 
 using namespace std;
 
+//https://stackoverflow.com/questions/9424173/find-the-smallest-amongst-3-numbers-in-c
+
 int main() {
 	cout << min(1,2) << endl; //1
 	cout << max(1,2) << endl; //2
+	//for multiple arguments, add {} around them!
+	cout << min({1,2,3,4}) << endl; //1
 	return 0;
 }
