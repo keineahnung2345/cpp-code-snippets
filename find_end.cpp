88fde705bdd1d76986dcbbc5ea9c0825265c12dc
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-22 14:55:00 +0800

    add distance calculation and result

--- find_end.cpp
+++ find_end.cpp
@@ -2,15 +2,23 @@
 #include <vector>
 #include <algorithm> //find_end
 
-using namespace std;
-
 int main()
 {
-    vector<int> v = {1, 6, 2, 3, 4, 5, 6, 2};
-    vector<int> tofind = {6};
-    
-    vector<int>::iterator it = find_end(v.begin(), v.end(), tofind.begin(), tofind.end());
-    
-    cout << "last 6 is the " << it - v.begin() << " th value in v" << endl;;
+    std::vector<int> v = {1, 6, 2, 3, 4, 5, 6, 2};
+    std::vector<int> tofind = {6};
+
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    std::vector<int>::iterator it = std::find_end(v.begin(), v.end(), tofind.begin(), tofind.end());
+    //if 6 is found
+    if(it != v.end()){
+        std::cout << "last 6 is the " << it - v.begin() << "th(index start from 0) value in v, " << v.end() - it << "th(index start from 1) value in reverse order." << std::endl;
+    }
     return 0;
 }
+
+/*
+1 6 2 3 4 5 6 2
+last 6 is the 6th(index start from 0) value in v, 2th(index start from 1) value in reverse order.
+*/
