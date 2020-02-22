f26a8efe2e5b65bce67cea1a4c32ead112316455
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-22 11:57:55 +0800

    add reverse sort

---
+++ vector_(reverse)_sort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+int main()
+{
+    std::vector<int> v = {3,6,2,8,1,9};
+
+    std::sort(v.begin(), v.end());
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    //1 2 3 6 8 9
+
+    std::sort(v.begin(), v.end(), std::greater<int>());
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    //9 8 6 3 2 1
+
+    return 0;
+}
