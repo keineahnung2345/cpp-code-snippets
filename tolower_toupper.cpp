9d58a2a41051fb9f16b9fb8550d13b0ba42578aa
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-23 18:18:25 +0800

    for string

--- tolower_toupper.cpp
+++ tolower_toupper.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <limits>
 
-using namespace std;
+//https://stackoverflow.com/questions/313970/how-to-convert-stdstring-to-lower-case
+//https://en.cppreference.com/w/cpp/string/byte/tolower
 
 int main() {
-	cout << tolower('A') << " " << (char)tolower('A') << endl; //97 a
-	cout << toupper('a') << " " << (char)toupper('a') << endl; //65 A
-	return 0;
+    std::cout << std::tolower('A') << " " << (char)std::tolower('A') << std::endl; //97 a
+    std::cout << std::toupper('a') << " " << (char)std::toupper('a') << std::endl; //65 A
+    std::string s = "Hello WORLD";
+    std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c){return std::tolower(c);});
+    std::cout << s << std::endl;
+    return 0;
 }
+
+/*
+97 a
+65 A
+hello world
+*/
