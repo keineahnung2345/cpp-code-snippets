e69e9decb60ae03446328b91cf69a80b6e103c78
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-23 15:03:17 +0800

    string_formatting.cpp

---
+++ string_formatting.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <sstream> //std::ostringstream
+#include <iomanip> //std::setw, std::setfill
+
+//https://stackoverflow.com/questions/6143824/add-leading-zeros-to-string-without-sprintf
+//https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
+
+int main(){
+    int hour = 3, minute = 1;
+    std::ostringstream ss;
+    ss << hour << ":" << std::setw(2) << std::setfill('0') << minute;
+    std::string str1 = ss.str();
+    std::cout << str1 << std::endl; //3:01
+    return 0;
+}
