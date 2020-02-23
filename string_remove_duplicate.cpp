6aadc98ef858aaab120abf6f81401fb41e802ef1
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-23 18:45:06 +0800

    string_remove_duplicate.cpp

---
+++ string_remove_duplicate.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+
+//https://stackoverflow.com/questions/8362094/replace-multiple-spaces-with-one-space-in-a-string
+
+int main(){
+    std::string mystr = "hello    world   good        bye";
+    char dup = ' '; //the duplicate character to be removed
+    std::cout << mystr << std::endl;
+    mystr.erase(
+        std::unique(mystr.begin(), mystr.end(),
+            [](const char& lhs, const char& rhs){return lhs == ' ' && (lhs == rhs);}
+        ),
+        mystr.end()
+    );
+    std::cout << mystr << std::endl;
+    return 0;
+}
+
+/*
+hello    world   good        bye
+hello world good bye
+*/
