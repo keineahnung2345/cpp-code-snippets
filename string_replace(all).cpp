23c5a66b220fabd0c6cce914445b55ede05ef592
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-23 18:32:54 +0800

    code for replace all

---
+++ string_replace(all).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string s = "Bob hit a ball, the hit ball flew far after it was hit.";
+    std::cout << "old string: " << s << std::endl;
+
+    std::string toReplace("hit");
+    std::string newText("split");
+   
+    size_t pos = s.find(toReplace);
+    //replace the first occurence
+    //NOTE THAT C++ std::string replace is IN-PLACE!!!
+    s.replace(pos, toReplace.length(), newText);
+    std::cout << "replace one: " << s << std::endl;
+
+    while(s.find(toReplace) != std::string::npos){
+        s.replace(s.find(toReplace), toReplace.length(), newText);
+    }
+    std::cout << "replace all: " << s << std::endl;
+    return 0;
+}
+
+/*
+old string: Bob hit a ball, the hit ball flew far after it was hit.
+replace one: Bob split a ball, the hit ball flew far after it was hit.
+replace all: Bob split a ball, the split ball flew far after it was split.
+*/
