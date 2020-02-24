592656370dabc1bdf5a74efd1427d18451ae2537
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-24 11:32:28 +0800

    remove "using namespace std;"

--- bit_length.cpp
+++ bit_length.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-using namespace std;
-
+  
 //https://stackoverflow.com/questions/29388711/c-how-to-get-length-of-bits-of-a-variable
 int main() {
-	unsigned int bits;
-	int ox;
-	unsigned int x;
-	cin >> ox;
-	x = ox; //convert to unsigned int
-	x = (x < 0)?(-x):x;
-	for(bits = 0; x != 0; bits++) x >>= 1;
-	cout << ox << ", " << bits << endl;
-	/**
-	 * 5, 3
-	 * -2, 32
-	**/
-	return 0;
+    unsigned int bits;
+    int ox;
+    unsigned int x;
+    std::cin >> ox;
+    x = ox; //convert to unsigned int
+    x = (x < 0)?(-x):x;
+    for(bits = 0; x != 0; bits++) x >>= 1;
+    std::cout << ox << ", " << bits << std::endl;
+    return 0;
 }
+
+/*
+5, 3
+-2, 32
+*/
