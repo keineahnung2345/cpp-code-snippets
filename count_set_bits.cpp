61677299e505eb18adf4328acafb2f731ae8621f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-24 11:45:24 +0800

    method 2

--- count_set_bits.cpp
+++ count_set_bits.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
-#include <bits/stdc++.h> 
+#include <bitset>
 
-using namespace std;
+int main(){
+    int x = 20;
 
-int main()
-{
-    bitset<5> b(20);
-    cout << b << endl; //10010
-    cout << b.count() << endl; //2
-    cout << b[0] << endl; //0, index start from behind
+    //Method 1
+    std::bitset<5> b(x);
+    std::cout << "bitset object: " << b << std::endl; //10010
+    std::cout << "its set bit count: " << b.count() << std::endl; //2
+    std::cout << "output in index order: ";
+    for(int i = 0; i < 5; i++){
+        std::cout << b[i]; //0, index start from behind
+    }
+    std::cout << std::endl;
 
+    //Method 2
+    x = 20;
+    int bits = 0;
+    while(x != 0){
+        bits += x&1;
+        x >>= 1;
+    }
+    std::cout << "its set bit count: " << bits << std::endl;
     return 0;
 }
+
+/*
+bitset object: 10100
+its set bit count: 2
+output in index order: 00101
+its set bit count: 2
+*/
