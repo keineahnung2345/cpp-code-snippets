2369ac5626b4b32566ddd23760ad4cd0e90948ac
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-24 10:19:51 +0800

    add less example

--- priority_queue.cpp
+++ priority_queue.cpp
@@ -2,8 +2,6 @@
 #include <queue> //priority_queue
 #include <vector>
 
-using namespace std;
-
 int main()
 {
     //https://en.cppreference.com/w/cpp/container/priority_queue
@@ -20,18 +18,37 @@ int main()
     but pq pop last element in the queue first,
     so when using pop, we will see ascending order
     **/
-     
-    priority_queue<int, vector<int>, greater<int>> pq;
-    vector<int> v = {1,7,2,9,3,0,4,5};
-    
+
+    std::vector<int> v = {1,7,2,9,3,0,4,5};
+
+    std::priority_queue<int, std::vector<int>, std::greater<int>> pq1;
+
     for(int e : v){
-        pq.push(e);
+        pq1.push(e);
     }
-    
-    while(!pq.empty()){
-        cout << pq.top() << " ";
-        pq.pop();
+
+    while(!pq1.empty()){
+        std::cout << pq1.top() << " ";
+        pq1.pop();
     }
+    std::cout << std::endl;
+
+    std::priority_queue<int, std::vector<int>, std::less<int>> pq2;
+
+    for(int e : v){
+        pq2.push(e);
+    }
+
+    while(!pq2.empty()){
+        std::cout << pq2.top() << " ";
+        pq2.pop();
+    }
+    std::cout << std::endl;
 
     return 0;
 }
+
+/*
+0 1 2 3 4 5 7 9
+9 7 5 4 3 2 1 0
+*/
