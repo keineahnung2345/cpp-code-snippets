3bcf902cc40dfba2e17b5167c62faaa15ac73518
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-27 18:25:36 +0800

    sum for long int

--- vector_sum.cpp
+++ vector_sum.cpp
@@ -2,13 +2,17 @@
 #include <vector>
 #include <numeric> //accumulate
 
-using namespace std;
+//https://stackoverflow.com/questions/46691506/summation-of-vector-containing-long-long-int-using-accumulation
 
 int main()
 {
-    vector<int> v = {1, 2, 3};
-    
-    cout << accumulate(v.begin(), v.end(), 0) << endl;
+    std::vector<int> v = {1, 2, 3};
+
+    std::cout << accumulate(v.begin(), v.end(), 0) << std::endl; //6
+
+    v = {1, 2147483647};
+    //std::cout << accumulate(v.begin(), v.end(), 0) << std::endl; //-2147483648
+    std::cout << accumulate(v.begin(), v.end(), 0L) << std::endl; //2147483648
 
     return 0;
 }
