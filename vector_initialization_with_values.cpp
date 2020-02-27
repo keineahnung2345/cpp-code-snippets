c7d5e73ae426cbb3d2a1585aa35c32e442578c75
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-27 17:52:03 +0800

    initialize with const

--- vector_initialization_with_values.cpp
+++ vector_initialization_with_values.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <iterator> //ostream_iterator
 
-using namespace std;
-
-//https://stackoverflow.com/questions/4268886/initialize-a-vector-array-of-strings
+//https://stackoverflow.com/questions/4268886/initialize-a-std::vector-array-of-strings
+//https://stackoverflow.com/questions/13110130/initialize-a-vector-to-zeros-c-c11
 
 int main() {
-    vector<int> a = {1, 2, 3, 4};
-    vector<int> b;
+    std::vector<int> a = {1, 2, 3, 4};
+    std::vector<int> b;
     b = {5,6,7};
+    std::vector<int> c(10, 1);
+    std::copy(c.begin(), c.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
     return 0;
 }
