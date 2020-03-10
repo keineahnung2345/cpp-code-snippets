41f6ae8b18627633841252cd42bbf9b45510103f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-10 22:05:53 +0800

    add source

--- vector_cumulative_product.cpp
+++ vector_cumulative_product.cpp
@@ -3,6 +3,8 @@
 #include <numeric> //accumulate
 #include <functional> //multiplies
 
+//https://stackoverflow.com/questions/29146175/c-multiplying-elements-in-a-vector
+
 int main(){
     std::vector<int> myvec = {1,2,3,4,5,6};
     int cumprod = std::accumulate(myvec.begin(), myvec.end(), 1, std::multiplies<int>());
