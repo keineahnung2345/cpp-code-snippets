635b38d0c72128dbaf2438050ffec4077432746b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-10 23:31:37 +0800

    remove "using namespace std;"

--- map_max_value.cpp
+++ map_max_value.cpp
@@ -3,24 +3,24 @@
 #include <utility> //pair
 #include <algorithm> //max_element
 
-using namespace std;
-
 //https://stackoverflow.com/questions/9370945/c-help-finding-the-max-value-in-a-map
 
 int main()
 {
-    map<int, int> mymap;
+    std::map<int, int> mymap;
     mymap[10] = 3;
     mymap[20] = 5;
     mymap[30] = 4;
     mymap[40] = 1;
     mymap[50] = 2;
-    
-    map<int, int>::iterator p = max_element(mymap.begin(), mymap.end(), [] (const pair<int, int> & p1, const pair<int, int> & p2) {
+
+    std::map<int, int>::iterator p = std::max_element(mymap.begin(), mymap.end(), [] (const std::pair<int, int> & p1, const std::pair<int, int> & p2) {
         return p1.second < p2.second;
     });
-    
-    cout << p->first << " " << p->second << endl;
-    
+
+    std::cout << p->first << " " << p->second << std::endl;
+
     return 0;
 }
+
+//20 5
