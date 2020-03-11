30efa61a4efb71f0f3fbf13985152a68b9aa1ca5
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-11 21:52:28 +0800

    split string by space

--- string_split.cpp
+++ string_split.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 
 //https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
+//https://www.geeksforgeeks.org/split-a-sentence-into-words-in-cpp/
 
 std::vector<std::string> string_split(std::string str, std::string delimiter){
     size_t pos = 0;
@@ -17,14 +19,46 @@ std::vector<std::string> string_split(std::string str, std::string delimiter){
     return result;
 }
 
+std::vector<std::string> string_split2(std::string str)
+{   
+    // Used to split string around spaces.
+    std::istringstream ss(str);
+    std::vector<std::string> result;
+    
+    do { 
+        std::string word;
+        ss >> word;  
+        result.push_back(word);
+    } while (ss);
+    
+    return result;
+}
+
 int main()
 {
     std::string str = "scott>=tiger>=mushroom";
     std::string delimiter = ">=";
-    
+ 
+    //Method 1
     for(std::string token : string_split(str, delimiter)){
         std::cout << token << std::endl;
     }
+    std::cout << "=====================" << std::endl;
+ 
+    //Method 2
+    str = "Geeks for Geeks";
+    std::vector<std::string> result = string_split2(str);
+    std::copy(result.begin(), result.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
 
     return 0;
 }
+
+/*
+tiger
+mushroom
+=====================
+Geeks
+for
+Geeks
+
+*/
