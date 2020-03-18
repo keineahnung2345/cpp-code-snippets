0c3988a47763dbd21b185af36fcfee0e021fd318
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-18 13:40:16 +0800

    input.cpp

---
+++ input.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int a;
+	int n = 2;
+    while(cin >> a){
+        cout << "Read number: " << a << endl;
+        if(--n == 0) break;
+    }
+	
+	string str;
+	while(getline(cin, str)){
+		cout << str << endl;
+	}
+	return 0;
+}
+
+/*
+Input:
+1 2
+hello goodbye
+hey yolo
+checkout here
+*/
+
+/*
+Output:
+Read number: 1
+Read number: 2
+
+hello goodbye
+hey yolo
+checkout here
+
+*/
