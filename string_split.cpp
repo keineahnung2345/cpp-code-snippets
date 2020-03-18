f50cf90eaa16aaf8257cdc02498932267e527eca
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-18 13:19:06 +0800

    string split style 2

--- string_split.cpp
+++ string_split.cpp
@@ -11,12 +11,27 @@ std::vector<std::string> string_split(std::string str, std::string delimiter){
     size_t pos = 0;
     std::string token;
     std::vector<std::string> result;
+    
+    //Style 1, need to deal with remaining string after the loop
+    /*
     while ((pos = str.find(delimiter)) != std::string::npos) {
         token = str.substr(0, pos);
         result.push_back(token);
         str.erase(0, pos + delimiter.length());
     }
     result.push_back(str);
+    */
+    
+    //Style 2, can deal with all tokens inside the while loop
+    while(true){
+        pos = str.find(delimiter);
+        //works even if pos is string::npos
+        token = str.substr(0, pos);
+        result.push_back(token);
+        if(pos == string::npos) break;
+        //pos+1 equals to 0, so the line below can't handle this situation
+        str.erase(0, pos+delimiter.length());
+    }
     return result;
 }
 
