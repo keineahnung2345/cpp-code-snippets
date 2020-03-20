1b88502e069fdf45c5b2f43cf71efcc54e9b259e
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-20 22:07:49 +0800

    0_int_long_float_double.cpp

---
+++ 0_int_long_float_double.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    0;    //int
+    0L;   //long
+  	0LL;  //long long
+  	0UL;  //unsigned long
+	  0.0;  //double
+  	0.0f; //float
+  	0.0L; //long double
+  	return 0;
+}
