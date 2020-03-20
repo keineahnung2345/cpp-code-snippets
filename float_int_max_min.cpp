0baa2cb112b8f8e4911149ddfaa1224ec9063186
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-20 21:31:55 +0800

    float max min

--- float_int_max_min.cpp
+++ float_int_max_min.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <climits>
+#include <climits> //INT_MAX
+#include <cfloat> //FLT_MAX
 
 using namespace std;
 
@@ -9,6 +10,8 @@ int main()
 {
     
     int a = INT_MAX, b = INT_MIN;
+    float c = FLT_MAX, d = FLT_MIN;
     cout << a << " " << b << endl; //2147483647 -2147483648 
+    cout << c << " " << d << endl; //3.40282e+38 1.17549e-38
     return 0;
 }
