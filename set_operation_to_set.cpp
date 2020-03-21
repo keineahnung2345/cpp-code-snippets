697e71951c747f337f3047051389ddbaaf5dc18b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-21 18:17:28 +0800

    swap

--- set_operation_to_set.cpp
+++ set_operation_to_set.cpp
@@ -6,13 +6,19 @@
 
 using namespace std;
 
+//https://stackoverflow.com/questions/16079362/how-to-compute-in-place-set-difference-of-two-multisets
+
 int main() {
     set<string> sA = {"a", "b", "c"};
     set<string> sB = {"a", "d", "e", "f"};
-    set<string> result;
+    set<string> tmp;
     set_symmetric_difference(sA.begin(), sA.end(), 
                                  sB.begin(), sB.end(),
-                                 inserter(result, result.begin()));
+                                 inserter(tmp, tmp.begin()));
+    //if we want the result to be stored in sA
+    sA.swap(tmp);
                                  
-    copy(result.begin(), result.end(), ostream_iterator<string>(cout, " "));
+    copy(sA.begin(), sA.end(), ostream_iterator<string>(cout, " "));
 }
+
+//b c d e f 
