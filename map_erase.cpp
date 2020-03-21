5df00e328cb65be52cd94c7fe5b027bd61d279c0
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-21 19:06:07 +0800

    map erase

--- map_erase.cpp
+++ map_erase.cpp
@@ -6,11 +6,20 @@ using namespace std;
 int main()
 {
     map<char, int> m;
+    map<char, int>::iterator it;
+    
     m['a'] = 3;
     m['b'] = 5;
     m['c'] = 7;
     
-    map<char, int>::iterator it = m.find('a');
+    cout << "original map..." << endl;
+    for(it = m.begin(); it != m.end(); it++){
+        cout << it->first << " " << it->second << endl;
+    }
+    
+    //map::erase method 1
+	cout << "erase a..." << endl;
+    it = m.find('a');
     
     if(it != m.end()){
         m.erase(it);
@@ -20,5 +29,25 @@ int main()
         cout << it->first << " " << it->second << endl;
     }
     
+    //map::erase method 2
+	cout << "continue to erase b..." << endl;
+    m.erase('b');
+    
+    for(it = m.begin(); it != m.end(); it++){
+        cout << it->first << " " << it->second << endl;
+    }
+    
     return 0;
 }
+
+/*
+original map...
+a 3
+b 5
+c 7
+erase a...
+b 5
+c 7
+continue to erase b...
+c 7
+*/
