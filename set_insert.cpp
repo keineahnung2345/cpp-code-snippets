78e522782b28362e2bdfad0d09baf66f5aee2a10
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-24 17:15:16 +0800

    correct the description about insert return value

--- set_insert.cpp
+++ set_insert.cpp
@@ -5,8 +5,12 @@
 int main(){
     std::set<int> myset = {1,2,3};
     std::pair<std::set<int>::iterator, bool> result = myset.insert(4);
-    std::cout << "4 already exist? " << result.second << std::endl;
+    std::cout << "4 [already exist/successfully inserted](0/1)? " << result.second << std::endl;
     result = myset.insert(4);
-    std::cout << "4 already exist? " << result.second << std::endl;
+    std::cout << "4 [already exist/successfully inserted](0/1)? " << result.second << std::endl;
     return 0;
 }
+/*
+4 [already exist/successfully inserted](0/1)? 1
+4 [already exist/successfully inserted](0/1)? 0
+*/
