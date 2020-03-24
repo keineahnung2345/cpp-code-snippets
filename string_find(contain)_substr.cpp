8e9d1ec3f8e57bcbdb16e83643f798cb11d0e74a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-24 17:20:58 +0800

    find substr from

--- string_find(contain)_substr.cpp
+++ string_find(contain)_substr.cpp
@@ -6,10 +6,21 @@ using namespace std;
 
 int main()
 {
-    string s1 = "hello world";
-    string s2 = "hello";
+    string s1 = "ABCABABC";
+    string s2 = "ABC";
     
-    if(s1.find(s2)!=string::npos){
-        cout << s1 << " contains " << s2 << endl;
+    size_t found = s1.find(s2);
+    if(found != string::npos){
+        cout << s1 << " contains " << s2 << ", starts from " << found << endl;
+    }
+    
+    found = s1.find(s2, found+1);
+    if(found != string::npos){
+        cout << s1 << " still contains " << s2 << ", starts from " << found << endl;
     }
 }
+
+/*
+ABCABABC contains ABC, starts from 0
+ABCABABC still contains ABC, starts from 5
+*/
