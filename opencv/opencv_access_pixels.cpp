898433f6a8c104314d31238b925c1da202d6c038
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-08 10:46:41 +0800

    add ref

--- opencv/opencv_access_pixels.cpp
+++ opencv/opencv_access_pixels.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+//https://stackoverflow.com/questions/17919399/accessing-pixel-value-of-gray-scale-image-in-opencv
+
 int main(){
     cv::Mat img = cv::imread("../Lenna.png", 0);
     for(int i = 0; i < img.rows; i++){
