6a2722ba182032ead4efc373bbd476037944baf6
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-08 09:33:50 +0800

    add ref

--- opencv/opencv_imshow.cpp
+++ opencv/opencv_imshow.cpp
@@ -2,6 +2,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
+//https://docs.opencv.org/2.4/doc/tutorials/introduction/display_image/display_image.html
+
 int main(){
     cv::Mat img = cv::imread("../Lenna.png", 0);
     cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE);// Create a window for display.
