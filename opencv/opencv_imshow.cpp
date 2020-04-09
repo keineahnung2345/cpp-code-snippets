6306b4dee249cdcc6a96be88847c6ac8e55a93c5
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-09 15:01:51 +0800

    make it a function

--- opencv/opencv_imshow.cpp
+++ opencv/opencv_imshow.cpp
@@ -4,11 +4,15 @@
 
 //https://docs.opencv.org/2.4/doc/tutorials/introduction/display_image/display_image.html
 
-int main(){
-    cv::Mat img = cv::imread("../Lenna.png", 0);
-    cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE);// Create a window for display.
-    cv::imshow( "Display window", img);                   // Show our image inside it.
+void Show(cv::Mat& img, std::string title = "Display Window"){
+    cv::namedWindow( title, cv::WINDOW_AUTOSIZE);// Create a window for display.
+    cv::imshow(title, img);                   // Show our image inside it.
     cv::waitKey(0);
     cv::destroyAllWindows();
+};
+
+int main(){
+    cv::Mat img = cv::imread("../Lenna.png", 0);
+    Show(img, "Lenna");
     return 0;
 }
