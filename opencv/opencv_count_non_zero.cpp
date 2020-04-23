febbf01f52bb28d52d4f1c18684d8031693bb242
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-23 10:51:10 +0800

    opencv_count_non_zero.cpp

---
+++ opencv/opencv_count_non_zero.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/hal/interface.h> //CV_8UC3
+#include <iostream>
+
+int main(){
+    //read in binary image, pixel values are either 0 or 255
+    cv::Mat img = cv::imread("binary.png", 0);
+    int height = img.rows, width = img.cols;
+    std::cout << "image size: " << height << " x " << width << std::endl;
+    std::cout << "There are " << cv::countNonZero(img) << " white pixels." << std::endl;
+    std::cout << "There are " << width*height - cv::countNonZero(img) << " black pixels." << std::endl;
+}
+
+/*
+image size: 426 x 653
+There are 242138 white pixels.
+There are 36040 black pixels.
+*/
