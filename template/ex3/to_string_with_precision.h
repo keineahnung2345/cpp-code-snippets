553b5033b7cf6772e4b54ecb66c5ef6e6abcea4b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-27 11:55:22 +0800

    #include "to_string_with_precision.tpp"

--- template/ex3/to_string_with_precision.h
+++ template/ex3/to_string_with_precision.h
@@ -1,4 +1,12 @@
-#include <string>
+// #include <string>
+#include <sstream>      // std::ostringstream
+/*
+Need to #include <sstream> in header file, otherwise: 
+error: invalid use of incomplete type ‘std::ostringstream {aka class std::__cxx11::basic_ostringstream<char>}’
+*/
 
 template <typename T>
 std::string to_string_with_precision(const T a_value, const int n = 6);
+
+// required to include its implementation
+#include "to_string_with_precision.tpp"
