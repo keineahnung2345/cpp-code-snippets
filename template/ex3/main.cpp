669cfb28261f404e6d45fd56ca33bd87b41d9914
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-27 11:54:28 +0800

    main.cpp

---
+++ template/ex3/main.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+#include <iostream>
+#include "to_string_with_precision.h"
+
+int main(){
+    std::cout << to_string_with_precision(M_PI, 2) << std::endl;
+    return 0;
+}
+
+//3.14
