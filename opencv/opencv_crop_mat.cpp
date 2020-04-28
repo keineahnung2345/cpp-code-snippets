481ff24bcb7ee7a166611b288bcd1ae0ef5eaa85
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-28 09:52:56 +0800

    include iostream

--- opencv/opencv_crop_mat.cpp
+++ opencv/opencv_crop_mat.cpp
@@ -1,11 +1,15 @@
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
 int main(){
     cv::Mat img = cv::imread("face.jpeg");
+    //x and w in left-right direction, y and h in top-down direction
     int x = 97, y = 0, w = 224 - 97, h = 118 - 0;
     cv::Mat roi = img(cv::Rect(x, y, w, h));
     std::cout << "original image size: " << img.size << std::endl;
     std::cout << "cropped image size: " << roi.size << std::endl;
+    std::cout << "cropped image rows: " << roi.rows << ", cols: " << roi.cols << std::endl;
     //original image size: 173 x 292
     //cropped image size: 118 x 127
+    //cropped image rows: 118, cols: 127
 }
