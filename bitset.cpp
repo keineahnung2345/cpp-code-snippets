22c18586bed3c7ac7134bc659408d1c0090c78ac
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-03 18:49:27 +0800

    bitset initialization

--- bitset.cpp
+++ bitset.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 #include <bitset>
-
+ 
 using namespace std;
-
+ 
 int main() {
-    bitset<5> bs;
-    //the index starts from the least significant bit
+	//the index starts from the least significant bit
+	//initialize 0th bit as 1
+    bitset<5> bs(1);
+    cout << "count: " << bs.count() << 
+    		", str: " << bs.to_string() << 
+    		", long: " << bs.to_ulong() << endl;
+ 
+ 
     bs.flip(2);
+    cout << "count: " << bs.count() << 
+    		", str: " << bs.to_string() << 
+    		", long: " << bs.to_ulong() << endl;
+ 
     bs.flip(4);
-    cout << bs.count() << endl;
-    cout << bs.to_string() << endl;
-    cout << bs.to_ulong() << endl;
+    cout << "count: " << bs.count() << 
+    		", str: " << bs.to_string() << 
+    		", long: " << bs.to_ulong() << endl;
     return 0;
 }
 
 /*
-2
-10100
-20
+count: 1, str: 00001, long: 1
+count: 2, str: 00101, long: 5
+count: 3, str: 10101, long: 21
 */
