049499021f7951d32e320b2f21921cb482582c73
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-06 22:49:39 +0800

    erase multiple elements

--- vector_erase.cpp
+++ vector_erase.cpp
@@ -3,16 +3,27 @@
 using namespace std;
 
 int main() {
-	vector<int> v = {1,2,3};
-	
-	for(int e : v) cout << e << " ";
-	cout << endl;
-	
-	//remove 2
-	v.erase(v.begin()+1);
-	
-	for(int e : v) cout << e << " ";
-	cout << endl;
-	
-	return 0;
+    vector<int> v = {1,2,3,4,5,6,7,8,9};
+    
+    for(int e : v) cout << e << " ";
+    cout << endl;
+    
+    //remove 8
+    v.erase(v.begin()+7);
+    
+    for(int e : v) cout << e << " ";
+    cout << endl;
+    
+    //remove 4,5
+    v.erase(v.begin()+3, v.begin()+5);
+    for(int e : v) cout << e << " ";
+    cout << endl;
+    
+    return 0;
 }
+
+/*
+1 2 3 4 5 6 7 8 9 
+1 2 3 4 5 6 7 9 
+1 2 3 6 7 9 
+*/
