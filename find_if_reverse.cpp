d473a97ee131eb1da4fb9d8eaac6c874a2372697
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-11 11:36:31 +0800

    add source

--- find_if_reverse.cpp
+++ find_if_reverse.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 
+//https://stackoverflow.com/questions/39165432/find-last-element-in-stdvector-which-satisfies-a-condition
+
 int main(){
     std::vector<int> v{1,2,42,42,63};
     auto result = std::find_if(v.rbegin(), v.rend(),
