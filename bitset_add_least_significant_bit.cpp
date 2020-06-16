7a357a34153df80abb86c113289e39614b40a466
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-16 15:16:21 +0800

    add ref

--- bitset_add_least_significant_bit.cpp
+++ bitset_add_least_significant_bit.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+//https://stackoverflow.com/questions/757059/position-of-least-significant-bit-that-is-set
+
 int main() {
     bitset<3> x(6);
     bitset<3> y = bitset<3>(x.to_ulong()-1);
