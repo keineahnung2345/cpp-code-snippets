d3c435c9d1dda5b7f662258438c635f5beb91994
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-18 17:28:36 +0800

    string_from_vector_of_int.cpp

---
+++ string_from_vector_of_int.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <sstream> //stringstream
+#include <iterator> //ostream_iterator
+#include <algorithm> //copy
+
+using namespace std;
+
+int main() {
+    vector<int> vec = {0, 1, 0};
+    stringstream result;
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(result, ""));
+    cout << result.str() << endl;
+    return 0;
+}
+
+//010
