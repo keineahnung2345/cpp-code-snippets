1fcabb7c206be4e2fdc43e1906d5c4343e11b1d4
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-07 17:24:27 +0800

    bitset_rightmost set bit.cpp

---
+++ bitset_rightmost set bit.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+//two's complement
+//https://www.electronics-tutorials.ws/binary/signed-binary-numbers.html
+//rightmost set bit
+//https://www.geeksforgeeks.org/position-of-rightmost-set-bit/
+//https://stackoverflow.com/questions/757059/position-of-least-significant-bit-that-is-set
+
+int main() {
+    bitset<3> x(6);
+    bitset<3> x_sub_1 = bitset<3>(x.to_ulong()-1);
+    bitset<3> x_comp_add_1 = bitset<3>((~x).to_ulong()+1);
+    cout << "x: " << x << ", x-1: " << x_sub_1 << endl;
+    //two way to get rightmost set bit
+    cout << x << "'s rightmost set bit: " << (x & ~x_sub_1) << " = " << x_comp_add_1 << endl;
+    cout << "remove " << x << "'s rightmost set bit: " << (x & x_sub_1) << endl;
+    return 0;
+}
