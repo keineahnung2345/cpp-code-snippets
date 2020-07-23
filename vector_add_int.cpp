0a27f060871a887588270fa0dc391cf4768d90b1
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-23 21:17:28 +0800

    transform

--- vector_add_int.cpp
+++ vector_add_int.cpp
@@ -7,12 +7,26 @@ using namespace std;
 //https://stackoverflow.com/questions/4461446/stl-way-to-add-a-constant-value-to-a-stdvector
 
 int main() {
-    int x = 5;
+    int addend = 5;
     vector<int> myvec = {1,2,3};
-    for_each(myvec.begin(), myvec.end(), [&x](int& e) { e+=x;});
+    
+    //Method 1:
+    for_each(myvec.begin(), myvec.end(), [&addend](int& e) { e+=addend;});
     for(int e : myvec){
         cout << e << " ";
     }
     cout << endl;
+    
+    //Method 2:
+    //The function allows for the destination range to be the same as one of the input ranges to make transformations in place.
+    transform(myvec.begin(), myvec.end(), myvec.begin(), [&addend](int e){return e+addend;});
+    for(int e : myvec){
+        cout << e << " ";
+    }
+    cout << endl;
+    
     return 0;
 }
+
+//6 7 8 
+//11 12 13 
