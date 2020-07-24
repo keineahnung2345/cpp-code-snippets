5e7043a904c66ff1d87e89924edf282fad56230f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-24 17:07:09 +0800

    any_of_all_of.cpp

--- any_of_all_of.cpp
+++ any_of_all_of.cpp
@@ -3,28 +3,38 @@
 #include <algorithm> //std::any_of
 
 //http://www.cplusplus.com/reference/algorithm/any_of/
+//http://www.cplusplus.com/reference/algorithm/all_of/
 
 int main() {
     std::vector<bool> vec = {false, true, false};
-    
+ 
     if(std::any_of(vec.begin(), vec.end(), [](const bool& b){return b;})){
         std::cout << "There is at least one true!" << std::endl;
     }else{
         std::cout << "They are all false!" << std::endl;
     }
-    
+ 
     vec = {false, false, false};
-    
+ 
     if(std::any_of(vec.begin(), vec.end(), [](const bool& b){return b;})){
         std::cout << "There is at least one true!" << std::endl;
     }else{
         std::cout << "They are all false!" << std::endl;
     }
-    
+ 
+    vec = {true, true, false};
+ 
+    if(std::all_of(vec.begin(), vec.end(), [](const bool& b){return b;})){
+        std::cout << "They are all true!" << std::endl;
+    }else{
+        std::cout << "There is at least one false!" << std::endl;
+    }
+ 
     return 0;
 }
 
 /*
 There is at least one true!
 They are all false!
+There is at least one false!
 */
