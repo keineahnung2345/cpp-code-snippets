b626f7b06be450d39d051ead65ec9c1a8da83f7f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-28 18:09:26 +0800

    vector_erase_in_loop.cpp

---
+++ vector_erase_in_loop.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> myvec = {3,6,2,9,5,1,0,7,4};
+    
+    for(vector<int>::iterator it = myvec.begin(); it != myvec.end(); ){
+        if(*it < 5){
+            myvec.erase(it);
+        }else{
+            ++it;
+        }
+    }
+    
+    for(int& e : myvec){
+        cout << e << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
+
+//6 9 5 7 
