030e54c2883c39c2ab56c34eba3fde34a5665c1b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-28 18:11:28 +0800

    add ref

--- vector_erase_in_loop.cpp
+++ vector_erase_in_loop.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+//https://stackoverflow.com/questions/8628951/remove-elements-of-a-vector-inside-the-loop
+
 int main() {
     vector<int> myvec = {3,6,2,9,5,1,0,7,4};
     
