c7f075c039011b09bed716cb3b142849d63adba6
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-13 16:55:10 +0800

    assign char* to string

--- string_from_char*.cpp
+++ string_from_char*.cpp
@@ -2,14 +2,27 @@
 #include <iostream>
 
 //https://www.geeksforgeeks.org/convert-character-array-to-string-in-c/
+//https://stackoverflow.com/questions/1195675/convert-a-char-to-stdstring
 
 int main(){
-    char a[] = { 'C', 'O', 'D', 'E' };
+    char a[] = { 'C', 'O', 'D', 'E', '\0'};
     char b[] = "geeksforgeeks";
+    char* c = "abc";
+    int l = 3;
 
     std::string s(a);
     std::cout << s << std::endl;
+    
     s = b;
+    std::cout << s << std::endl;
+    
+    s.assign(c, l);
     std::cout << s << std::endl;
     return 0;
 }
+
+/*
+CODE
+geeksforgeeks
+abc
+*/
