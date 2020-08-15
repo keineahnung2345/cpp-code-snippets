b796cf203548a41f5bc3e0df17d1b1d12661ec5c
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-15 21:44:40 +0800

    copy_if_python_filter.cpp

---
+++ copy_if_python_filter.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm> //copy_if
+#include <vector>
+ 
+using namespace std;
+ 
+int main() {
+    vector<int> myvec = {1,2,3,4,5,6};
+ 
+    vector<int> tmp;
+    copy_if(myvec.begin(), myvec.end(), 
+            back_inserter(tmp), 
+            [](int& e){
+                return e&1;} 
+           );
+    for(int& e : tmp){
+        cout << e << " ";
+    }
+    cout << endl;
+ 
+    return 0;
+}
+
+//1 3 5
