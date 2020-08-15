a712e6a1d7515c2901e4b78fd6a70d5e6e6a741a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-15 12:29:59 +0800

    replace

--- regex.cpp
+++ regex.cpp
@@ -3,6 +3,7 @@
 #include <regex>
 
 //http://www.cplusplus.com/reference/regex/regex_match/
+//http://www.cplusplus.com/reference/regex/regex_replace/
 
 int main(){
     std::string mystr = "hello, good bye and hello";
@@ -10,5 +11,17 @@ int main(){
     if(std::regex_match(mystr, pattern)){
         std::cout << mystr << " matches the pattern" << std::endl;
     }
+    
+    mystr = "<DIV>This is the first line <![CDATA[<div>]]></DIV>";
+    std::cout << "before: " << mystr << std::endl;
+    pattern = std::regex("<!\\[CDATA\\[.*?\\]\\]>");
+    mystr = regex_replace(mystr, pattern, "c");
+    std::cout << "after: " << mystr << std::endl;
     return 0;
 }
+
+/*
+hello, good bye and hello matches the pattern
+before: <DIV>This is the first line <![CDATA[<div>]]></DIV>
+after: <DIV>This is the first line c</DIV>
+*/
