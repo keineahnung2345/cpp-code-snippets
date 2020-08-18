e90da07249d697bca4a72b61f2d92283a280e08b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-18 16:09:16 +0800

    add ref

--- map_initialize.cpp
+++ map_initialize.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+//https://stackoverflow.com/questions/138600/initializing-a-static-stdmapint-int-in-c
+
 int main()
 {
     map<int, char> m = {{1, 'a'}, {3, 'b'}, {5, 'c'}, {7, 'd'}};
