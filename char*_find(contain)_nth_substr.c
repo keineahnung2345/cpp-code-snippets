d8aad133910ca9432ae3091a6bc53638734eac86
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-20 18:17:26 +0800

    char*_find(contain)_nth_substr.c

---
+++ char*_find(contain)_nth_substr.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int strpos(char* haystack, char* needle, int nth){
+     //nth is 1-based
+    char* res = haystack;
+    for(int i = 1; i <= nth; i++){
+        res = strstr(res, needle);
+        if (!res){
+            return -1;
+        }else if(i != nth){
+            res++;
+        }
+    }
+    return res - haystack;
+}
+
+int main()
+{
+    char* haystack = "The rain in Spain falls mainly on the plain";
+    char* needle = "ain";
+    int nth = 2;
+    
+    int pos = strpos(haystack, needle, 2);
+    
+    printf("%dnd '%s' in '%s': %d", nth, needle, haystack, pos);
+    
+    return 0;
+}
+
+//2nd 'ain' in 'The rain in Spain falls mainly on the plain': 14
