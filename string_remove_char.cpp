a06bc563198a58248db90afa883b8a20b2a6ca4d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-26 21:59:18 +0800

    string_remove_char.cpp

---
+++ string_remove_char.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+//https://stackoverflow.com/questions/3882304/replace-remove-character-in-string
+//http://www.cplusplus.com/reference/algorithm/remove/
+
+int main()
+{
+    string mystr = "2020/08/26";
+    cout << mystr << " -> ";
+    //the 3rd argument can only be a char, not a string
+    auto it = remove(mystr.begin(), mystr.end(), '/');
+    string mystr_removed = string(mystr.begin(), it);
+    cout << mystr_removed << endl;
+    
+    return 0;
+}
+
+//2020/08/26 -> 20200826
