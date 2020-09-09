61365888ac2ed098a01af03eff0d7cabd81732cf
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-09 16:38:16 +0800

    back_inserter

--- transform_python_map.cpp
+++ transform_python_map.cpp
@@ -9,14 +9,25 @@ using namespace std;
 
 int main() {
     vector<int> myvec = {1,2,3};
+    vector<int> myemptyvec;
     set<int> myset = {1,2,3}, tmp;
     
+    //target vector's size is the same as source vector's
     transform(myvec.begin(), myvec.end(), myvec.begin(), [](int& x){return x+1;});
+    
+    //target vector is empty, need to use "back_inserter"
+    transform(myvec.begin(), myvec.end(), back_inserter(myemptyvec), [](int& x){return x+1;});
+    
     //cannot add "&" in lambda function!
     transform(myset.begin(), myset.end(), inserter(tmp, tmp.end()), [](int x){return x+1;});
     swap(myset, tmp);
     
     for(int e : myvec){
+        cout << e << " ";
+    }
+    cout << endl;
+    
+    for(int e : myemptyvec){
         cout << e << " ";
     }
     cout << endl;
@@ -28,3 +39,7 @@ int main() {
     
     return 0;
 }
+
+//2 3 4 
+//3 4 5 
+//2 3 4 
