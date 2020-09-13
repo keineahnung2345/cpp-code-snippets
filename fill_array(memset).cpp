6042d14a1919519eced33bbcfec8393c7202ca6a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-13 21:28:43 +0800

    array of bool and char

--- fill_array(memset).cpp
+++ fill_array(memset).cpp
@@ -5,15 +5,16 @@
 using namespace std;
 
 //https://stackoverflow.com/questions/3586774/fill-multidimensional-array-elements-with-0s
+//https://stackoverflow.com/questions/9215210/how-to-memset-an-array-of-bools
 
 int main() {
     int a[3][4][5];
     
-    //works
-    //memset(a, -1, sizeof(a));
+    //memset not work for int!!
+    // memset(a, 123, sizeof(a));
     
     //works
-    fill(a[0][0], a[0][0]+3*4*5, -1);
+    fill(a[0][0], a[0][0]+3*4*5, 123);
     
     for(int i = 0; i < 3; ++i){
         for(int j = 0; j < 4; ++j){
@@ -24,5 +25,32 @@ int main() {
         }
         cout << endl;
     }
+    
+    char c[20];
+    
+    //memset works for char
+    memset(c, '$', sizeof(c));
+    
+    //works
+    // fill(begin(c), end(c), '$');
+    
+    for(int i = 0; i < 20; ++i){
+        cout << c[i] << " ";
+    }
+    cout << endl;
+    
+    char b[20];
+    
+    //memset works for bool
+    memset(b, true, sizeof(bool) * sizeof(b));
+    
+    //works
+    // fill(begin(b), end(b), true);
+    
+    for(int i = 0; i < 20; ++i){
+        cout << (b[i] ? '1' : '0') << " ";
+    }
+    cout << endl;
+    
     return 0;
 }
