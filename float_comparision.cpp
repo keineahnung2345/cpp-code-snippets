dc9bd4b5eaef10c6498100784e10c5fb791900d4
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-28 21:25:35 +0800

    float compare

--- float_comparision.cpp
+++ float_comparision.cpp
@@ -4,13 +4,18 @@
 //#include <math.h>
 #include <cmath> //std::fabs
 #include <limits> //std::numeric_limits<float>::epsilon()
+#include <algorithm> //std::max
+
+//https://stackoverflow.com/questions/17333/what-is-the-most-effective-way-for-float-and-double-comparison
 
 int main(){
-    float a = 0.000001, b = 0.000002;
-    if(std::fabs(a-b) > std::numeric_limits<float>::epsilon() * 10){
-        printf("a and b's difference is larger than epsilon * 10!\n");
+    float a = 2.345678f, b = 2.345677f;
+    if(std::fabs(a-b) > std::numeric_limits<float>::epsilon() * std::max(std::fabs(a), std::fabs(b))){
+        printf("a and b are not equal\n");
     }else{
-        printf("a and b's difference is not larger than epsilon * 10!\n");
+        printf("a and b are close enough\n");
     }
     return 0;
 }
+
+//a and b are not equal
