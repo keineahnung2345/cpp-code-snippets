4c832c784fbbab8d20b37efc01a70115dfe19217
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-08 18:22:09 +0800

    LLONG_MAX

--- int_float_double_max_min.cpp
+++ int_float_double_max_min.cpp
@@ -7,8 +7,9 @@
 //https://en.cppreference.com/w/cpp/types/numeric_limits
 
 int main(){
-	//C-style
+    //C-style
     std::cout << INT_MAX << " " << INT_MIN << std::endl; //2147483647 -2147483648 
+    std::cout << LLONG_MAX << " " << LLONG_MIN << std::endl; //9223372036854775807 -9223372036854775808
     std::cout << FLT_MAX << " " << FLT_MIN << std::endl; //3.40282e+38 1.17549e-38
     std::cout << DBL_MAX << " " << DBL_MIN << std::endl; //1.79769e+308 2.22507e-308
     //C++-style
@@ -19,6 +20,7 @@ int main(){
 
 /*
 2147483647 -2147483648
+-9223372036854775808 9223372036854775807
 3.40282e+38 1.17549e-38
 1.79769e+308 2.22507e-308
 3.40282e+38 1.17549e-38 -3.40282e+38
