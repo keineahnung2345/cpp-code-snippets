942107476b1ea75d5c61b5e30acf4559140d5a5f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-30 16:57:02 +0800

    color to grayscale image

--- opencv/opencv_imwrite_convert_color.cpp
+++ opencv/opencv_imwrite_convert_color.cpp
@@ -3,10 +3,14 @@
 
 //g++ cv_write.cpp `pkg-config --cflags --libs opencv4` -std=c++11
 
+//https://stackoverflow.com/questions/10344246/how-can-i-convert-a-cvmat-to-a-gray-scale-in-opencv
+
 int main(){
     cv::Mat src = cv::imread("face.jpg");
     cv::Mat dst;
     cv::cvtColor(src, dst, cv::COLOR_RGB2BGR);
+    //color to grayscale image
+    //cv::cvtColor(src, dst, cv::COLOR_BGR2GRAY);
     cv::imwrite("face_reverted.jpg", dst);
     return 0;
 }
