69a33dd001044e6ca3cd3ac931004368f2f0c74a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2021-07-25 09:46:45 +0800

    typeid for vector

--- typeid_operator_and_pointer_declaration.cpp
+++ typeid_operator_and_pointer_declaration.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <typeinfo>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -12,15 +13,21 @@ int main()
     int* i3, i4; //i3 will be treated as pointer, but i4 not
     float f1;
     double d1;
-    std::string s1;
+    string s1;
+    vector<int> vi;
+    vector<vector<int>> vvi;
+    vector<string> vs;
 
-    cout << typeid(i1).name() << endl; //Pi
-    cout << typeid(i2).name() << endl; //Pi
-    cout << typeid(i3).name() << endl; //Pi
-    cout << typeid(i4).name() << endl; //i
-    cout << typeid(f1).name() << endl; //f
-    cout << typeid(d1).name() << endl; //d
-    cout << typeid(s1).name() << endl; //NSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE
+    cout << typeid(i1).name() << endl;  //Pi
+    cout << typeid(i2).name() << endl;  //Pi
+    cout << typeid(i3).name() << endl;  //Pi
+    cout << typeid(i4).name() << endl;  //i
+    cout << typeid(f1).name() << endl;  //f
+    cout << typeid(d1).name() << endl;  //d
+    cout << typeid(s1).name() << endl;  //NSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE
+    cout << typeid(vi).name() << endl;  //St6vectorIiSaIiEE
+    cout << typeid(vvi).name() << endl; //St6vectorIS_IiSaIiEESaIS1_EE
+    cout << typeid(vs).name() << endl;  //St6vectorINSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEESaIS5_EE
     
     return 0;
 }
