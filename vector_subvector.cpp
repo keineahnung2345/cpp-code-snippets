eab7de46507593a31f5324e763fe00a1e3f765bc
keineahnung2345 <mimifasosofamire1123@gmail.com>
2021-07-28 15:48:39 +0800

    add note

--- vector_subvector.cpp
+++ vector_subvector.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <vector>
 
+//https://stackoverflow.com/questions/421573/best-way-to-extract-a-subvector-from-a-vector
+
 using namespace std;
 
 int main()
 {
     vector<int> myvec = {1,2,3,4,5,6,7,8,9};
+    // note that endIter cannot be omitted
     vector<int> mysubvec(myvec.begin()+3, myvec.begin()+5);
     
     for(int& e : mysubvec){
