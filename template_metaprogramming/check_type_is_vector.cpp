19e2d0d7848cb22796bcdc8997ea9c0561127373
keineahnung2345 <mimifasosofamire1123@gmail.com>
2021-07-29 10:06:39 +0800

    check_type_is_vector.cpp

---
+++ template_metaprogramming/check_type_is_vector.cpp
@@ -0,0 +1,24 @@
+#include <type_traits>
+#include <iostream>
+#include <vector>
+
+//https://stackoverflow.com/questions/16337610/how-to-know-if-a-type-is-a-specialization-of-stdvector
+
+template<typename>
+struct is_std_vector : std::false_type {};
+
+template<typename T, typename A>
+struct is_std_vector<std::vector<T,A>> : std::true_type {};
+
+int main()
+{
+    typedef std::vector<int> vec;
+    typedef int not_vec;
+    std::cout << "std::vector<int> is std::vector? " << is_std_vector<vec>::value << std::endl;
+    std::cout << "int is std::vector? " << is_std_vector<not_vec>::value << std::endl;
+}
+
+/**
+std::vector<int> is std::vector? 1
+int is std::vector? 0
+**/
