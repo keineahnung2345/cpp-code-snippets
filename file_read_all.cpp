52098143766a457a171cef67e03b853c69784e16
keineahnung2345 <mimifasosofamire1123@gmail.com>
2021-08-02 17:37:41 +0800

    stringstream read all file

--- file_read_all.cpp
+++ file_read_all.cpp
@@ -1,11 +1,25 @@
 #include <fstream>
 #include <string>
+#include <iostream>
 
 //https://stackoverflow.com/questions/18398167/how-to-copy-a-txt-file-to-a-char-array-in-c
+//https://stackoverflow.com/questions/2602013/read-whole-ascii-file-into-c-stdstring
 
 int main(){
     std::ifstream in("xxx.txt");
-    std::string contents((std::istreambuf_iterator<char>(in)), 
+    
+    // Method 1
+    std::string content1((std::istreambuf_iterator<char>(in)), 
         std::istreambuf_iterator<char>());
+    
+    // Method 2
+    std::stringstream ss;
+    ss << in.rdbuf();
+    std::string content2 = ss.str();
+    
+    std::cout << content1 << std::endl;
+    std::cout << content2 << std::endl;
+    
+    in.close();
     return 0;
 }
