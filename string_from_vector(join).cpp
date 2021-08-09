d812b2803f6aa6fcb995ede39645c0a89c8b90da
keineahnung2345 <mimifasosofamire1123@gmail.com>
2021-08-09 11:02:17 +0800

    join method 2

--- string_from_vector(join).cpp
+++ string_from_vector(join).cpp
@@ -1,10 +1,11 @@
 #include <vector>
 #include <iostream>
 #include <sstream> //std::ostringstream
+#include <numeric> //accumulate
 
 //g++ join.cpp -std=c++11
-//https://stackoverflow.com/questions/6097927/is-there-a-way-to-implement-analog-of-pythons-separator-join-in-c
 
+//https://stackoverflow.com/questions/6097927/is-there-a-way-to-implement-analog-of-pythons-separator-join-in-c
 template <typename Iter>
 std::string join(Iter begin, Iter end, std::string const& separator)
 {
@@ -18,6 +19,17 @@ std::string join(Iter begin, Iter end, std::string const& separator)
     return result.str();
 }
 
+//https://stackoverflow.com/questions/9277906/stdvector-to-string-with-custom-delimiter
+//can only be used with container of string
+template <typename Iter>
+std::string join2(Iter begin, Iter end, std::string const& separator) {
+    return std::accumulate(begin, end, std::string(),
+        [&separator](std::string& ss, std::string& s)
+        {
+            return ss.empty() ? s : ss + separator + s;
+        });
+}
+
 int main(){
     std::vector<std::string> vs = {"monkey", "tiger", "horse"};
     std::vector<float> vf = {2.333, 4.2322, 7.2125, 553.3};
@@ -25,6 +37,10 @@ int main(){
     std::cout << join(vs.begin(), vs.end(), ", ") << std::endl;
     std::cout << join(vf.begin(), vf.end(), ", ") << std::endl;
     std::cout << join(vb.begin(), vb.end(), ", ") << std::endl;
+    
+    std::vector<std::string> vs = { "hello", "goodbye", "thanks" };
+    std::string res = join(vs.begin(), vs.end(), "-");
+    std::cout << res << std::endl; //hello-goodbye-thanks
 }
 
 /*
