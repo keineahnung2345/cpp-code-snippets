2931d672b2fc4070b0eb86f9d9b07c501b4333cf
keineahnung2345 <mimifasosofamire1123@gmail.com>
2021-08-30 10:00:29 +0800

    add uint8_t enum class for testing

--- enum_to_underlying.cpp
+++ enum_to_underlying.cpp
@@ -7,13 +7,27 @@ enum class Fruit{
     DURIAN
 };
 
+enum class Vehicle : uint8_t {
+    CAR = 0,
+    AIRPLANE,
+    SHIP
+};
+
 template <typename E>
 constexpr typename std::underlying_type<E>::type to_underlying(E e) noexcept {
     return static_cast<typename std::underlying_type<E>::type>(e);
 };
 
 int main() {
-    std::cout << "Fruit::COCONUT's underlying value: " << to_underlying(Fruit::COCONUT) << std::endl;
+    std::cout << "Fruit::COCONUT's underlying value: " << to_underlying(Fruit::COCONUT)
+      << ", underlying type: " << typeid(to_underlying(Fruit::COCONUT)).name() << std::endl;
+    std::cout << "Vehicle::AIRPLANE's underlying value: " << to_underlying(Vehicle::AIRPLANE)
+      << ", underlying type: " << typeid(to_underlying(Vehicle::AIRPLANE)).name() << std::endl;
     //Fruit::COCONUT's underlying value: 2
     return 0;
 }
+
+/**
+Fruit::COCONUT's underlying value: 2, underlying type: i
+Vehicle::AIRPLANE's underlying value: , underlying type: h
+**/
