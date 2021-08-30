ba67e4a62044166a147be0cb4e0fe3d8b14d6bdb
keineahnung2345 <mimifasosofamire1123@gmail.com>
2021-08-30 09:55:01 +0800

    timestamp.cpp

---
+++ timestamp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+std::string get_timestamp() {
+    time_t now = time(NULL);
+    struct tm* ltm = localtime(&now);
+    char buffer[80];
+ 
+    strftime(buffer, 80, "%Y-%m-%d-%H-%M-%S", ltm);
+ 
+    return std::string(buffer);
+}
+ 
+int main() {
+    std::cout << "current timestamp: " << get_timestamp() << std::endl;
+    //current timestamp: 2021-08-30-01-53-30
+    return 0;
+}
