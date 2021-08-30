7f894069d7e82e86ad8a8630a1db5e65415ba1ab
keineahnung2345 <mimifasosofamire1123@gmail.com>
2021-08-30 10:09:28 +0800

    check_type_is_string.cpp

---
+++ template_metaprogramming/check_type_is_string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+//https://stackoverflow.com/questions/60929056/c-detect-if-a-type-is-a-string-object
+template <typename T>
+struct is_std_string : std::is_same<std::remove_cv_t<std::remove_reference_t<T>>, std::string> { };
+
+int main() {
+    std::cout << "std::string is a string? " << std::boolalpha << is_std_string<std::string>::value << std::endl;
+    std::cout << "int is a string? " << std::boolalpha << is_std_string<int>::value << std::endl;
+    std::cout << "std::vector<int> is a string? " << std::boolalpha << is_std_string<std::vector<int>>::value << std::endl;
+    return 0;
+}
+
+/**
+std::string is a string? 1
+int is a string? 0
+std::vector<int> is a string? 0
+**/
