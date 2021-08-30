d638aa83f6ff5e6c8ab6cf0111f9f4c815fa18dd
keineahnung2345 <mimifasosofamire1123@gmail.com>
2021-08-30 10:12:12 +0800

    add ref

--- template_metaprogramming/check_type_is_scalar.cpp
+++ template_metaprogramming/check_type_is_scalar.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <type_traits> //std::is_scalar
 
+//https://www.geeksforgeeks.org/is_scalar-template-in-c/
+
 int main() {
     std::cout << "std::string is a scalar? " << std::boolalpha << std::is_scalar<std::string>::value << std::endl;
     std::cout << "int is a scalar? " << std::boolalpha << std::is_scalar<int>::value << std::endl;
