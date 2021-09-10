41e6b84abc0c80a0d52e2f91927294f05ef9e18c
keineahnung2345 <mimifasosofamire1123@gmail.com>
2021-09-10 10:25:32 +0800

    add ref

--- file_basename_boost.cpp
+++ file_basename_boost.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <boost/filesystem.hpp>
 
+//https://stackoverflow.com/questions/8520560/get-a-file-name-from-a-path
+
 int main(){
     std::string basename = boost::filesystem::path("C:/MyDirectory/MyFile.bat").filename().string();
     return 0;
