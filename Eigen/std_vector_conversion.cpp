0a4c540da6a7742eb0fc560825d73bed39cdfa4d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2021-10-15 09:20:09 +0800

    std_vector_conversion.cpp

--- Eigen/std_vector_conversion.cpp
+++ Eigen/std_vector_conversion.cpp
@@ -4,17 +4,25 @@
 #include <Eigen/Dense>
 
 //https://stackoverflow.com/questions/26094379/typecasting-eigenvectorxd-to-stdvector
+//https://stackoverflow.com/questions/17036818/initialise-eigenvector-with-stdvector
 
 using namespace std;
 
 int main(int argc, char** argv) {
     Eigen::Vector3f mat(1.2, 3.4, 5.6);
     vector<float> vec(mat.data(), mat.data() + mat.size());
-    for (const float& e : vec) {
+    for (float& e : vec) {
         cout << e << " ";
+        e += 1;
     }
     cout << endl;
     //1.2 3.4 5.6
+    
+    mat = Eigen::Vector3f(vec.data());
+    for (size_t i = 0; i < 3; ++i){
+        cout << mat(i) << " ";
+    }
+    cout << endl;
 
     return 0;
 }
