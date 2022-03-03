616efd7bc25ee67d2051ac369ea63cd715262e9a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2022-03-03 16:10:16 +0800

    fix random int denominator

--- random_int.cpp
+++ random_int.cpp
@@ -2,12 +2,15 @@
 
 using namespace std;
 
+// https://stackoverflow.com/questions/5008804/generating-random-integer-from-a-range
+
 int main()
 {
     srand(1); //set random seed for reproducible result
-    // this will generate numbers from 3 to 3+5=8
+    // this will generate numbers from 3(min) to 8(max)
+    int min = 3, max = 8;
     for(int i = 0; i < 10; i++){
-        cout << rand()%5+3 << " ";
+        cout << min + (rand() % static_cast<int>(max - min + 1)) << " ";
     }
     return 0;
 }
