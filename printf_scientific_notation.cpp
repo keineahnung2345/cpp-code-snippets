8998ee9c25adb3dfcbc7a0414780afea36471bb4
keineahnung2345 <mimifasosofamire1123@gmail.com>
2022-05-05 09:10:35 +0800

    Create printf_scientific_notation.cpp

---
+++ printf_scientific_notation.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    printf("%.10e\n", sqrt(3.14));
+    printf("%f\n", sqrt(3.14));
+    printf("%lf\n", sqrt(3.14));
+    return 0;
+}
+
+/*
+1.7720045147e+00
+1.772005
+1.772005
+*/
