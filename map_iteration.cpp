f4e800bf0ab50215db5f5362e4e9b234dbe2abfe
keineahnung2345 <mimifasosofamire1123@gmail.com>
2022-08-24 16:00:22 +0800

    add method 2

--- map_iteration.cpp
+++ map_iteration.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include <map>
 
-using namespace std;
+int main() {
+    std::map<char, int> m;
 
-int main()
-{
-    map<char, int> m;
-    
     m['a'] = 2;
     m['b'] = 5;
     m['c'] = 8;
 
-    //https://stackoverflow.com/questions/1063453/how-to-display-map-contents
-    for (const auto &p : m) {
+    // Method 1
+    // https://stackoverflow.com/questions/1063453/how-to-display-map-contents
+    for(const auto &p : m) {
         std::cout << "m[" << p.first << "] = " << p.second << '\n';
+    }
+
+    // Method 2
+    // https://stackoverflow.com/questions/1443793/iterate-keys-in-a-c-map
+    for(const auto &[key, value] : m) {
+        std::cout << "m[" << key << "] = " << value << '\n';
     }
 
     return 0;
 }
-
