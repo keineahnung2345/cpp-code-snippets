338bb31b68a9c0880126da789a676c13f8af7ad0
keineahnung2345 <mimifasosofamire1123@gmail.com>
2023-07-21 14:02:54 +0800

    Create timestamp_to_unix_time.cpp

---
+++ timestamp_to_unix_time.cpp
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <iomanip>  // std::get_time
+#include <iostream>
+#include <sstream>
+
+unsigned long long getUnixTime(std::chrono::system_clock::time_point p0 =
+                                   std::chrono::system_clock::time_point{
+                                       std::chrono::seconds{ 0 } }) {
+    if(p0 ==
+       std::chrono::system_clock::time_point{ std::chrono::seconds{ 0 } }) {
+        p0 = std::chrono::system_clock::now();
+    }
+    return std::chrono::duration_cast<std::chrono::seconds>(
+               p0.time_since_epoch())
+        .count();
+}
+
+unsigned long long timestamp2UnixTime(std::string timestamp) {
+    std::tm           timeinfo = {};
+    std::stringstream ss(timestamp);
+    ss >> std::get_time(&timeinfo, "%a %b %d %H:%M:%S %Y");
+    time_t epoch = mktime(&timeinfo);
+    auto   p0    = std::chrono::system_clock::from_time_t(epoch);
+    return getUnixTime(p0);
+}
+
+int main() {
+    std::cout << timestamp2UnixTime("Fri Jul 21 09:27:58 2023") << std::endl;
+    // 1689931678
+    return 0;
+}
