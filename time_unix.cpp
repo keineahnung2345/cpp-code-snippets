1f9b222fd4de6dab6f0e0d523bae795fe37064ee
keineahnung2345 <mimifasosofamire1123@gmail.com>
2023-07-21 11:59:39 +0800

    Create time_unix.cpp

---
+++ time_unix.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <chrono>
+
+unsigned long long getUnixTime(std::chrono::system_clock::time_point p0 =
+                                   std::chrono::system_clock::time_point{
+                                       std::chrono::seconds{ 0 } }) {
+    if(p0 ==
+       std::chrono::system_clock::time_point{ std::chrono::seconds{ 0 } }) {
+        p0 = std::chrono::system_clock::now();
+    }
+    return std::chrono::duration_cast<std::chrono::seconds>(
+               p0.time_since_epoch())
+        .count();
+}
+
+int main() {
+    std::cout << getUnixTime() << std::endl; // 1689911880
+    return 0;
+}
