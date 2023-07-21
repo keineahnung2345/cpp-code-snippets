978504959e4b77d637943d0c9b38a5d5790f8335
keineahnung2345 <mimifasosofamire1123@gmail.com>
2023-07-21 15:50:25 +0800

    Create crc32.cpp

---
+++ crc32.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include <Eigen/Dense>
+
+std::string getCrc32(const std::string& str) {
+    boost::crc_32_type result;
+    result.process_bytes(str.data(), str.length());
+    return (std::stringstream() << std::hex << result.checksum()).str();
+}
+
+template <typename T>
+std::string getCrc32(const T& value) {
+    return getCrc32((std::stringstream() << value).str());
+}
+
+int main() {
+    Eigen::Matrix4f m = Eigen::Matrix4f::Zero();
+    std::cout << getCrc32(m) << std::endl; // 9e4d0581
+    return 0;
+}
