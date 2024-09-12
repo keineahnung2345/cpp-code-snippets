b33b6b2ebe0daac6e8e2bced3bd179c0ecebf137
keineahnung2345 <mimifasosofamire1123@gmail.com>
2024-09-12 11:44:29 +0800

    erase remove_if

--- vector_erase_remove_idiom(remove all occurrences).cpp
+++ vector_erase_remove_idiom(remove all occurrences).cpp
@@ -1,26 +1,38 @@
+#include <algorithm>  //remove
 #include <iostream>
 #include <vector>
-#include <algorithm> //remove
 
 using namespace std;
 
+// https://en.cppreference.com/w/cpp/algorithm/remove
+// https://stackoverflow.com/questions/347441/erasing-elements-from-a-vector
+// https://stackoverflow.com/questions/34624223/how-to-erase-elements-from-a-vector-based-on-a-specific-condition-in-c11
 
-//https://en.cppreference.com/w/cpp/algorithm/remove
-//https://stackoverflow.com/questions/347441/erasing-elements-from-a-vector
+int main() {
+    vector<char> v = { 'a', 'b', 'c', 'a', 'd', 'b' };
 
-int main()
-{
-    vector<char> v = {'a', 'b', 'c', 'a', 'd', 'b'};
-    
-    //erase-remove idiom
-    v.erase(remove(v.begin(), v.end(), 'b'), v.end());
-    
-    for(auto e : v){
+    vector<char> v_wo_b = v;
+    // erase-remove idiom
+    v_wo_b.erase(remove(v_wo_b.begin(), v_wo_b.end(), 'b'), v_wo_b.end());
+
+    for(auto e : v_wo_b) {
         cout << e << " ";
     }
     cout << endl;
-    
+    // a c a d
+
+    vector<char> v_larger_than_b = v;
+    // erase-remove idiom
+    v_larger_than_b.erase(
+        remove_if(v_larger_than_b.begin(), v_larger_than_b.end(),
+                  [](char c) { return c <= 'b'; }),
+        v_larger_than_b.end());
+
+    for(auto e : v_larger_than_b) {
+        cout << e << " ";
+    }
+    cout << endl;
+    // c d
+
     return 0;
 }
-
-//a c a d
